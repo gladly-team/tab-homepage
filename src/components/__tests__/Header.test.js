abc1c42667280a5f3d7d847cdce2b2d26d079f02
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-10 16:01:30 -0700

    Add snapshot test

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 
 describe('Header', () => {
   it('renders without error', () => {
     const Header = require('../Header').default
     shallow(<Header />)
+  })
+
+  it('matches expected snapshot', function() {
+    const Header = require('../Header').default
+    const wrapper = shallow(<Header />)
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
