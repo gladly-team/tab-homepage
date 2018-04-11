7d45b5cad3aa3a6f2802c499a687eeba249da6b7
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-11 11:49:53 -0700

    Different install button text depending on browser and OS

--- src/components/InstallButton.js
+++ src/components/InstallButton.js
@@ -1,14 +1,62 @@
 import React from 'react'
 import Button from 'material-ui/Button'
+import detectBrowser from 'browser-detect'
 
 class InstallButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // One of: 'chrome', 'firefox', or 'other'
+      browser: 'other',
+      mobile: false,
+    }
+  }
+
+  componentWillMount() {
+    this.detectBrowser()
+  }
+
+  detectBrowser() {
+    const browserInfo = detectBrowser()
+    var browser = 'other'
+    switch (browserInfo.name) {
+      case 'chrome':
+        browser = 'chrome'
+        break
+      case 'chromium':
+        browser = 'chrome'
+        break
+      case 'firefox':
+        browser = 'firefox'
+        break
+      default:
+        break
+    }
+    const mobile = browserInfo.mobile ? true : false
+    this.setState({
+      browser: browser,
+      mobile: mobile,
+    })
+  }
+
   onClick() {
     console.log('Button clicked')
   }
 
   getButtonText() {
-    // TODO: customize to browser and device
-    return 'Add to Chrome'
+    // Customize text to browser and device
+    if (this.state.mobile) {
+      return 'Get it Now'
+    } else {
+      switch (this.state.browser) {
+        case 'chrome':
+          return 'Add to Chrome'
+        case 'firefox':
+          return 'Add to Firefox'
+        default:
+          return 'Get it Now'
+      }
+    }
   }
 
   render() {
--- src/components/__tests__/InstallButton.test.js
+++ src/components/__tests__/InstallButton.test.js
@@ -1,18 +1,96 @@
 /* eslint-env jest */
 
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
+
+jest.mock('browser-detect')
+
+const createMockBrowserInfo = (browser = 'chrome', mobile = false) => {
+  return {
+    name: browser,
+    version: '58.0.3029',
+    versionNumber: 58.03029,
+    mobile: mobile,
+    os: 'Windows NT 10.0',
+  }
+}
+
+afterEach(() => {
+  jest.resetModules()
+})
 
 describe('InstallButton', () => {
   it('renders without error', () => {
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo())
+
     const InstallButton = require('../InstallButton').default
     shallow(<InstallButton />)
   })
 
   it('matches expected snapshot', function() {
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo())
+
     const InstallButton = require('../InstallButton').default
     const wrapper = shallow(<InstallButton />)
     expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('has correct text for desktop Chrome', () => {
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('chrome', false))
+
+    const InstallButton = require('../InstallButton').default
+    const wrapper = mount(<InstallButton />)
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .text()
+    ).toEqual('Add to Chrome')
+  })
+
+  it('has correct text for desktop Firefox', () => {
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('firefox', false))
+
+    const InstallButton = require('../InstallButton').default
+    const wrapper = mount(<InstallButton />)
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .text()
+    ).toEqual('Add to Firefox')
+  })
+
+  it('has correct text for desktop unsupported browser', () => {
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('other', false))
+
+    const InstallButton = require('../InstallButton').default
+    const wrapper = mount(<InstallButton />)
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .text()
+    ).toEqual('Get it Now')
+  })
+
+  it('has correct text for mobile browser', () => {
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('chrome', true))
+
+    const InstallButton = require('../InstallButton').default
+    const wrapper = mount(<InstallButton />)
+    expect(
+      wrapper
+        .find('button')
+        .first()
+        .text()
+    ).toEqual('Get it Now')
   })
 })
