72eb8f25a7efd8e11511b8e24d5e27e0af99b98d
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-24 16:47:35 -0700

    Add data and display of quarterly financials

--- src/pages/financials.js
+++ src/pages/financials.js
@@ -1,13 +1,76 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Paper from 'material-ui/Paper'
 import TextPageContent from 'components/TextPageContent'
 
-const FinancialsPage = () => (
-  <div>
-    <TextPageContent>
-      <h1>Financials</h1>
-      <div>TODO</div>
-    </TextPageContent>
-  </div>
-)
+const FinancialsPage = ({ data }) => {
+  const quarterlyFinancialsEdges = data.allFinancialsYaml.edges
+  return (
+    <div>
+      <TextPageContent>
+        <h1>Financials</h1>
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-start',
+          }}
+        >
+          {quarterlyFinancialsEdges.map(quarter => {
+            return (
+              <a
+                href={quarter.node.pdfUrl}
+                key={`Q${quarter.node.quarter}${quarter.node.year}`}
+                style={{ margin: 14 }}
+              >
+                <Paper
+                  style={{
+                    width: 140,
+                    height: 140,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  {`Q${quarter.node.quarter} ${quarter.node.year}`}
+                </Paper>
+              </a>
+            )
+          })}
+        </div>
+      </TextPageContent>
+    </div>
+  )
+}
+
+FinancialsPage.propTypes = {
+  data: PropTypes.shape({
+    allFinancialsYaml: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            quarter: PropTypes.number.isRequired,
+            year: PropTypes.number.isRequired,
+            pdfUrl: PropTypes.string.isRequired,
+          }),
+        })
+      ),
+    }),
+  }),
+}
+
+export const query = graphql`
+  query QuarterlyFinancialsQuery {
+    allFinancialsYaml {
+      edges {
+        node {
+          quarter
+          year
+          pdfUrl
+        }
+      }
+    }
+  }
+`
 
 export default FinancialsPage
