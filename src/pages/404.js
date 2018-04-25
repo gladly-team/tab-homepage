99c0d421a6d838ea379963788e8a0f0b677790af
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-25 14:25:32 -0700

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import Button from 'material-ui/Button'
+import Link from 'components/Link'
+import { homeURL } from 'utils/navigation'
+import Cake from '@material-ui/icons/Cake'
+import { secondaryMainColor } from 'themes/theme'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div
+    style={{
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        maxWidth: 440,
+        padding: 20,
+      }}
+    >
+      <h1>Oops! Nothing here.</h1>
+      <p style={{ marginBottom: 0, padding: '0px 20px' }}>
+        Sorry about that! You probably weren't looking for cake, but have some
+        cake anyway.
+      </p>
+      <Cake
+        style={{
+          height: 40,
+          width: 40,
+          margin: 10,
+          color: secondaryMainColor,
+        }}
+      />
+      <Link to={homeURL} style={{ margin: 18 }}>
+        <Button variant="raised" color="primary" size="large">
+          Head back home
+        </Button>
+      </Link>
+    </div>
   </div>
 )
 
