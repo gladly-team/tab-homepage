b524743448658229e0c4b42b2dc47a97759d9535
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-25 16:11:55 -0700

    Set canonical URL in meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,89 +15,97 @@ import Footer from 'components/Footer'
 import { MuiThemeProvider } from 'material-ui/styles'
 import theme from '../themes/theme'
 
-// TODO: meta tag with dynamic route
-// <link rel="canonical" href={``} />
-// https://github.com/gatsbyjs/gatsby/issues/3449#issuecomment-365370644
+// Note that layouts will be deprecated in Gatsby V2:
+// https://github.com/gatsbyjs/gatsby/issues/3830
 
 // Use flexbox to make sure the footer sticks to the bottom of the page:
 // https://css-tricks.com/couple-takes-sticky-footer/#article-header-id-3
-const Layout = ({ children, data }) => (
-  <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh',
-      }}
-    >
-      <div
-        style={{
-          flex: '1 0 auto',
-        }}
-      >
-        <Helmet
-          title={data.site.siteMetadata.title}
-          defaultTitle={data.site.siteMetadata.title}
-        >
-          <meta
-            name="description"
-            content={data.site.siteMetadata.descriptionLong}
-          />
-          <meta name="keywords" content={data.site.siteMetadata.keywords} />
-          <meta
-            property="og:url"
-            content={`https://${data.site.siteMetadata.domain}/`} // FIXME: dynamic route
-          />
-          <meta
-            property="og:title"
-            content={data.site.siteMetadata.metaTagCallToAction}
-          />
-          <meta
-            property="og:description"
-            content={data.site.siteMetadata.descriptionShort}
-          />
-          <meta
-            property="og:image"
-            content={data.site.siteMetadata.metaTagImage}
-          />
-          <meta
-            name="twitter:title"
-            content={data.site.siteMetadata.metaTagCallToAction}
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:description"
-            content={data.site.siteMetadata.descriptionShort}
-          />
-          <meta
-            name="twitter:site"
-            content={data.site.siteMetadata.twitterHandle}
-          />
-          <meta
-            name="twitter:creator"
-            content={data.site.siteMetadata.twitterHandle}
-          />
-          <meta
-            name="twitter:image:src"
-            content={data.site.siteMetadata.metaTagImage}
-          />
-          <meta name="twitter:domain" content={data.site.siteMetadata.domain} />
-        </Helmet>
-
-        <Header siteTitle={data.site.siteMetadata.title} />
+class Layout extends React.Component {
+  render() {
+    const { data, location } = this.props
+    const absoluteUrl = `https://${data.site.siteMetadata.domain}${
+      location.pathname
+    }`
+    return (
+      <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
         <div
           style={{
-            paddingTop: 0,
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100vh',
           }}
         >
-          {children()}
-          }
+          <div
+            style={{
+              flex: '1 0 auto',
+            }}
+          >
+            <Helmet
+              title={data.site.siteMetadata.title}
+              defaultTitle={data.site.siteMetadata.title}
+            >
+              <meta
+                name="description"
+                content={data.site.siteMetadata.descriptionLong}
+              />
+              <meta name="keywords" content={data.site.siteMetadata.keywords} />
+              <link rel="canonical" href={absoluteUrl} />
+              <meta property="og:url" content={absoluteUrl} />
+              <meta
+                property="og:title"
+                content={data.site.siteMetadata.metaTagCallToAction}
+              />
+              <meta
+                property="og:description"
+                content={data.site.siteMetadata.descriptionShort}
+              />
+              <meta
+                property="og:image"
+                content={data.site.siteMetadata.metaTagImage}
+              />
+              <meta
+                name="twitter:title"
+                content={data.site.siteMetadata.metaTagCallToAction}
+              />
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta
+                name="twitter:description"
+                content={data.site.siteMetadata.descriptionShort}
+              />
+              <meta
+                name="twitter:site"
+                content={data.site.siteMetadata.twitterHandle}
+              />
+              <meta
+                name="twitter:creator"
+                content={data.site.siteMetadata.twitterHandle}
+              />
+              <meta
+                name="twitter:image:src"
+                content={data.site.siteMetadata.metaTagImage}
+              />
+              <meta
+                name="twitter:domain"
+                content={data.site.siteMetadata.domain}
+              />
+            </Helmet>
+
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <div
+              style={{
+                paddingTop: 0,
+              }}
+            >
+              {this.props.children()}
+              }
+            </div>
+          </div>
+          <Footer style={{ flexShrink: 0 }} />
         </div>
-      </div>
-      <Footer style={{ flexShrink: 0 }} />
-    </div>
-  </MuiThemeProvider>
-)
+      </MuiThemeProvider>
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
@@ -114,6 +122,9 @@ Layout.propTypes = {
         twitterHandle: PropTypes.string.isRequired,
       }),
     }),
+  }),
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
   }),
 }
 
