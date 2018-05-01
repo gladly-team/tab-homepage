fb65edaa814eff5930990a62b5a3949118644c57
Kevin Jennison <kevin.jennison1@gmail.com>
2018-05-01 10:10:47 -0700

    Resize images on skinny screens

--- src/utils/layout.js
+++ src/utils/layout.js
@@ -1,4 +1,4 @@
 // Sizing (px) for layouts and media queries
 export const maxPageWidth = 1480
-export const mediumScreenWidth = 1040
+export const mediumScreenWidth = 1000
 export const skinnyScreenWidth = 760 // probably a phone
