6e21f9627ce664ec149ceace380ad8a23a3adb6b
Kevin Jennison <kevin.jennison1@gmail.com>
2018-05-01 15:01:15 -0700

    Debugging build

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,26 @@ import Link from 'gatsby-link'
 import logoWithText from 'img/logo-with-text.svg'
 
 const Header = () => (
-  <div
-    style={{
-      paddingTop: 20,
-      paddingBottom: 20,
-      paddingLeft: 40,
-      paddingRight: 40,
-    }}
-  >
+  <div>
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'flex-start',
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 40,
+        paddingRight: 40,
       }}
     >
-      <Link to="/">
-        <img src={logoWithText} style={{ height: 40 }} />
-      </Link>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Link to="/">
+          <img src={logoWithText} style={{ height: 40 }} />
+        </Link>
+      </div>
+      <span>Tab for a Cause</span>
     </div>
   </div>
 )
