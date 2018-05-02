83336500940c1b5e131faa0ee196131cb7dae9aa
Kevin Jennison <kevin.jennison1@gmail.com>
2018-05-02 16:29:25 -0700

    Add placeholder pages

---
+++ src/pages/adblockers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TextPageContent from 'components/TextPageContent'
+import TextPageHeader from 'components/TextPageHeader'
+
+class AdblockersPage extends React.Component {
+  render() {
+    return (
+      <TextPageContent>
+        <TextPageHeader>Whitelisting Your Adblocker</TextPageHeader>
+        <div>TODO: info goes here!</div>
+      </TextPageContent>
+    )
+  }
+}
+
+export default AdblockersPage
---
+++ src/pages/jobs.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TextPageContent from 'components/TextPageContent'
+import TextPageHeader from 'components/TextPageHeader'
+
+class JobsPage extends React.Component {
+  render() {
+    return (
+      <TextPageContent>
+        <TextPageHeader>Jobs</TextPageHeader>
+        <div>TODO: jobs go here!</div>
+      </TextPageContent>
+    )
+  }
+}
+
+export default JobsPage
