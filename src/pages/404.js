824d2e702b6a3d9cc98218c91d1c9842169d62a5
Kevin Jennison <kevin.jennison1@gmail.com>
2018-05-02 15:24:22 -0700

    Fix for 404 page bug

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,50 +1,83 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Button from 'material-ui/Button'
 import Link from 'components/Link'
 import { homeURL } from 'utils/navigation'
+import { withTheme } from 'material-ui/styles'
 import Cake from '@material-ui/icons/Cake'
 import { secondaryMainColor } from 'themes/theme'
 
-const NotFoundPage = () => (
-  <div
-    style={{
-      height: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        maxWidth: 440,
-        padding: 20,
-      }}
-    >
-      <h1>Oops! Nothing here.</h1>
-      <p style={{ marginBottom: 0, padding: '0px 20px' }}>
-        Sorry about that! You probably weren't looking for cake, but have some
-        cake anyway.
-      </p>
-      <Cake
+// We're running into one of these problems or a related
+// problem:
+// "404 page component not mounted"
+//   https://github.com/gatsbyjs/gatsby/issues/2223
+// "404 page renders only html"
+//   https://github.com/gatsbyjs/gatsby/issues/2878
+//
+// It looks like Gatsby routing might mount the 404
+// component before the parent index.js layout:
+// https://github.com/gatsbyjs/gatsby/issues/2223#issuecomment-383410514
+// This breaks material-ui components that expect the
+// MUI theme to exist at the root of the tree.
+// To work around this, we check if the MUI theme exists
+// before rendering.
+class NotFoundPage extends React.Component {
+  render() {
+    if (!this.props.theme.palette) {
+      return null
+    }
+    return (
+      <div
         style={{
-          height: 40,
-          width: 40,
-          margin: 10,
-          color: secondaryMainColor,
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
-      />
-      <Link to={homeURL} style={{ margin: 18 }}>
-        <Button variant="raised" color="primary" size="large">
-          Head back home
-        </Button>
-      </Link>
-    </div>
-  </div>
-)
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            maxWidth: 440,
+            padding: 20,
+          }}
+        >
+          <h1>Oops! Nothing here.</h1>
+          <p style={{ marginBottom: 0, padding: '0px 20px' }}>
+            Sorry about that! You probably weren't looking for cake, but have
+            some cake anyway.
+          </p>
+          <Cake
+            style={{
+              height: 40,
+              width: 40,
+              margin: 10,
+              color: secondaryMainColor,
+            }}
+          />
+          <Link to={homeURL} style={{ margin: 18 }}>
+            <Button variant="raised" color="primary" size="large">
+              Head back home
+            </Button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
-export default NotFoundPage
+NotFoundPage.propTypes = {
+  theme: PropTypes.shape({
+    palette: PropTypes.object,
+  }).isRequired,
+}
+
+NotFoundPage.defaultProps = {
+  theme: {},
+}
+
+export default withTheme()(NotFoundPage)
