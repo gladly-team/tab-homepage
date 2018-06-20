c3edcc592a3e6a99dc65910e0c8dd45aef9b35fa
Kevin Jennison <kevin.jennison1@gmail.com>
2018-06-20 11:40:17 -0700

    Use argument to deploy script to specify S3 target

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,24 @@
 # Sync built files with appropriate cache headers.
 # https://www.gatsbyjs.org/docs/caching/
 
+S3_TARGET=$1
+if [ -z "$S3_TARGET" ];
+  then
+    echo "You must pass an S3 bucket as an argument to this script.";
+    exit 1
+fi
+
 # AWS CLI include/exclude filters:
 # https://docs.aws.amazon.com/cli/latest/reference/s3/index.html#use-of-exclude-and-include-filters
 
 # HTML. Do not cache.
-aws s3 sync public/ s3://dev-tab-website --region us-west-2 \
+aws s3 sync public/ $S3_TARGET --region us-west-2 \
   --exclude "*" --include "*.html" \
   --cache-control no-cache \
   --metadata-directive REPLACE
 
 # Static files. Cache for long term.
-aws s3 sync public/ s3://dev-tab-website --region us-west-2 \
+aws s3 sync public/ $S3_TARGET --region us-west-2 \
   --exclude "*" --include "static/*" \
   --cache-control max-age=31536000 \
   --metadata-directive REPLACE
@@ -22,7 +29,7 @@ aws s3 sync public/ s3://dev-tab-website --region us-west-2 \
 # Note that Gatsby cannot yet cache Javascript:
 # https://www.gatsbyjs.org/docs/caching/#javascript
 # https://github.com/gatsbyjs/gatsby/issues/2538
-aws s3 sync public/ s3://dev-tab-website --region us-west-2 \
+aws s3 sync public/ $S3_TARGET --region us-west-2 \
   --include "*" --exclude "*.html" --exclude "static/*" \
   --cache-control no-cache \
   --metadata-directive REPLACE
