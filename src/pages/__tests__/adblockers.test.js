15efb576881a39730e43384f58a761615791208a
Kevin Jennison <kevin.jennison1@gmail.com>
2018-06-22 12:07:24 -0700

    Add adblocker whitelisting instructions

--- src/pages/__tests__/adblockers.test.js
+++ src/pages/__tests__/adblockers.test.js
@@ -3,6 +3,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Helmet from 'react-helmet'
+import { getTestIdSelector } from 'utils/test-utils'
+import Button from 'material-ui/Button'
 
 const props = {}
 
@@ -33,5 +35,31 @@ describe('adblockers whitelisting page', () => {
     expect(elem.prop('content')).toBe(
       'Learn how to whitelist your adblocker to raise money for charity with every browser tab you open.'
     )
+  })
+
+  it('shows the expected number of adblocker buttons', () => {
+    const AdblockersPage = require('../adblockers').default
+    const wrapper = shallow(<AdblockersPage {...props} />)
+    expect(wrapper.find(Button).length).toBe(4)
+  })
+
+  it('does not show any adblocker instructions by default', () => {
+    const AdblockersPage = require('../adblockers').default
+    const wrapper = shallow(<AdblockersPage {...props} />)
+    const instructionsContainerElem = wrapper.find(
+      getTestIdSelector('adblocker-instructions-container')
+    )
+    expect(instructionsContainerElem.children().length).toBe(0)
+  })
+
+  it('shows instructions after clicking an adblocker button', () => {
+    const AdblockersPage = require('../adblockers').default
+    const wrapper = shallow(<AdblockersPage {...props} />)
+    const adblockerButton = wrapper.find(Button).first()
+    adblockerButton.simulate('click')
+    const instructionsContainerElem = wrapper.find(
+      getTestIdSelector('adblocker-instructions-container')
+    )
+    expect(instructionsContainerElem.children().length).toBeGreaterThan(0)
   })
 })
--- src/pages/adblockers.js
+++ src/pages/adblockers.js
@@ -4,12 +4,154 @@ import TextPageContent from 'components/TextPageContent'
 import TextPageHeader from 'components/TextPageHeader'
 import Divider from 'material-ui/Divider'
 import { lightestTextColor } from 'themes/theme'
+import Button from 'material-ui/Button'
+import { secondaryMainColor, secondaryContrastTextColor } from 'themes/theme'
+
+const adblockers = [
+  {
+    name: 'AdBlock Plus',
+    code: 'adblock-plus',
+    instructionsElem: (
+      <div>
+        <h3>Chrome</h3>
+        <ul>
+          <li>
+            <b>
+              On <em>this</em> page (not a new tab)
+            </b>, click on the AdBlock Plus icon (stop sign) on the top right of
+            your browser
+          </li>
+          <li>
+            Click "Enabled on this site" until you see "Disabled on this site"
+          </li>
+        </ul>
+
+        <h3>Firefox</h3>
+        <ul>
+          <li>
+            <b>
+              On <em>this</em> page (not a new tab)
+            </b>, click the AdBlock Plus icon (stop sign) on the top right of
+            your browser
+          </li>
+          <li>Click "Disable on tab.gladly.io"</li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    name: 'uBlock',
+    code: 'ublock',
+    instructionsElem: (
+      <div>
+        <h3>Chrome</h3>
+        <ul>
+          <li>
+            <b>
+              On a new tab (<em>not</em> this page)
+            </b>, click on the uBlock Origin icon (red shield) on the top right
+            of your browser
+          </li>
+          <li>Click the "power" button</li>
+        </ul>
+
+        <h3>Firefox</h3>
+        <ul>
+          <li>
+            <b>
+              On a new tab (<em>not</em> this page)
+            </b>, click on the uBlock Origin icon (red shield) on the top right
+            of your browser
+          </li>
+          <li>Click the "power" button</li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    name: 'Adblock',
+    code: 'adblock',
+    instructionsElem: (
+      <div>
+        <h3>Chrome</h3>
+        <ul>
+          <li>
+            <b>
+              On <em>this</em> page (not a new tab)
+            </b>, click on the Adblock icon (a hand) on the top right corner of
+            your browser
+          </li>
+          <li>Click "Don't run on pages on this domain"</li>
+          <li>Click "Exclude"</li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    name: 'Ghostery',
+    code: 'ghostery',
+    instructionsElem: (
+      <div>
+        <h3>Chrome</h3>
+        <div>
+          A bug in Ghostery makes it impossible to whitelist Tab for a Cause on
+          Chrome. We're talking with Ghostery to resolve this.
+        </div>
+        <p>
+          In the meantime, you can switch ad blockers (<a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en">
+            uBlock Origin
+          </a>{' '}
+          is a good open-source option) or disable Ghostery altogether.
+        </p>
+
+        <h3>Firefox</h3>
+        <ul>
+          <li>
+            Click on the Ghostery icon (a ghost) on the top right of your
+            browser
+          </li>
+          <li>Click "Trust Site"</li>
+        </ul>
+      </div>
+    ),
+  },
+]
 
 class AdblockersPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedAdblockerCode: null,
+    }
+  }
+
+  changeSelectedAdblocker(adblockerCode) {
+    this.setState({
+      selectedAdblockerCode: adblockerCode,
+    })
+  }
+
   render() {
     const openGraphTitle = 'Whitelisting Your Adblocker'
     const openGraphDescription =
       'Learn how to whitelist your adblocker to raise money for charity with every browser tab you open.'
+
+    // Get the instructions element for the selected adblocker.
+    var instructions = null
+    const selectedAdblockerCode = this.state.selectedAdblockerCode
+    if (selectedAdblockerCode) {
+      const adblockerInfo = adblockers.find(
+        ab => ab.code === selectedAdblockerCode
+      )
+      if (adblockerInfo) {
+        if (!adblockerInfo.instructionsElem) {
+          throw new Error(
+            `Adblocker ${adblockerInfo.name} is missing whitelist instructions.`
+          )
+        }
+        instructions = adblockerInfo.instructionsElem
+      }
+    }
     return (
       <TextPageContent>
         <Helmet title={'Whitelisting Your Adblocker'}>
@@ -29,10 +171,48 @@ class AdblockersPage extends React.Component {
           email us at contact@gladly.io
         </p>
         <Divider
-          style={{ backgroundColor: lightestTextColor, marginBottom: 20 }}
+          style={{
+            backgroundColor: lightestTextColor,
+            marginTop: 30,
+            marginBottom: 30,
+          }}
         />
-        <div>
+        <div style={{ marginTop: 30, marginBottom: 30 }}>
           <h2>What ad blocker do you use?</h2>
+          {adblockers.map(adblocker => {
+            const selected = selectedAdblockerCode === adblocker.code
+            return (
+              <Button
+                key={adblocker.code}
+                variant="flat"
+                color="secondary"
+                onClick={this.changeSelectedAdblocker.bind(
+                  this,
+                  adblocker.code
+                )}
+                style={Object.assign(
+                  {
+                    marginLeft: 8,
+                    marginRight: 8,
+                    color: selected
+                      ? secondaryContrastTextColor
+                      : secondaryMainColor,
+                  },
+                  selected && {
+                    background: secondaryMainColor,
+                  }
+                )}
+              >
+                {adblocker.name}
+              </Button>
+            )
+          })}
+        </div>
+        <div
+          data-test-id={'adblocker-instructions-container'}
+          style={{ marginTop: 30, marginBottom: 30 }}
+        >
+          {instructions}
         </div>
       </TextPageContent>
     )
