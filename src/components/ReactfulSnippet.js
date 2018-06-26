e02cb2420e92936b61957b1fa866aa620525e016
Kevin Jennison <kevin.jennison1@gmail.com>
2018-06-26 10:56:31 -0700

    Only load Reactful on component mount

--- src/components/ReactfulSnippet.js
+++ src/components/ReactfulSnippet.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 class ReactfulSnippet extends React.Component {
+  componentDidMount() {
+    if (!window) {
+      return
+    }
+    var reactful_client_id = '329608'
+    window._rctfl = window._rctfl || { c: [] }
+    window._rctfl.c.push(reactful_client_id)
+    var el = window.document.createElement('script')
+    el.async = 1
+    el.src = '//visitor.reactful.com/dist/main.rtfl.js'
+    window.document.getElementsByTagName('head')[0].appendChild(el)
+  }
+
   render() {
-    ;(function() {
-      var reactful_client_id = '329608'
-      window._rctfl = window._rctfl || { c: [] }
-      window._rctfl.c.push(reactful_client_id)
-      var el = window.document.createElement('script')
-      el.async = 1
-      el.src = '//visitor.reactful.com/dist/main.rtfl.js'
-      window.document.getElementsByTagName('head')[0].appendChild(el)
-    })()
     return null
   }
 }
