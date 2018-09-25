c378f9295f877d73b73379a84a59aa0183da0698
Kevin Jennison <kevin.jennison1@gmail.com>
2018-09-24 20:05:16 -0700

    Add Google GST (gtag.js) to head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,34 @@
+/* globals exports process */
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+import React from 'react'
+import { oneLine } from 'common-tags'
+
+// Google GST (gtag.js):
+// https://github.com/gatsbyjs/gatsby/issues/8341
+exports.onRenderBody = ({ setHeadComponents }) => {
+  const GST_ID = 'AW-1013744060'
+  if (process.env.NODE_ENV === `production`) {
+    setHeadComponents([
+      <script
+        key="google-gst-js"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GST_ID}`}
+      />,
+      <script
+        key="google-gst-init"
+        dangerouslySetInnerHTML={{
+          __html: oneLine`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GST_ID}');`,
+        }}
+      />,
+    ])
+  }
+}
