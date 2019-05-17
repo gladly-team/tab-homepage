a1e7d577605073661a3077398455d836dd25c64d
Kevin Jennison <kevin.jennison1@gmail.com>
2019-05-17 15:28:49 -0700

    Remove Babel polyfill and don't mix commonJS/ES6

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,11 @@
-/* globals exports */
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// https://github.com/gatsbyjs/gatsby/issues/3314#issuecomment-353648634
-import 'babel-polyfill'
-
 // https://github.com/gatsbyjs/gatsby/issues/2177#issuecomment-382280801
-exports.onClientEntry = () => {
+export const onClientEntry = () => {
   // Don't need to do anything here, but if you don't
   // export something, the import won't work.
 }
