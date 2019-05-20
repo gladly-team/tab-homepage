f8cff0f390e2dd38d4207c1b3270da808684de5c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-05-20 15:33:53 -0700

    Remove SSR plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,10 +19,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // Server-side rendering for material-ui, which prevents
-    // style flickering:
-    // https://github.com/gatsbyjs/gatsby/issues/2116#issuecomment-329996699
-    `gatsby-plugin-jss`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
