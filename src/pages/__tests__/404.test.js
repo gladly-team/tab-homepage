36ba5cc83be66819db48cf2e2e9a8f16ae68bd64
Kevin Jennison <kevin.jennison1@gmail.com>
2019-05-21 10:25:11 -0700

    Fix some tests

--- src/pages/__tests__/404.test.js
+++ src/pages/__tests__/404.test.js
@@ -1,8 +1,7 @@
 /* eslint-env jest */
 
 import React from 'react'
-// import { shallow } from 'enzyme'
-import { createShallow } from '@material-ui/core/test-utils'
+import { shallow } from 'enzyme'
 import Helmet from 'react-helmet'
 
 const getMockProps = () => ({
@@ -16,36 +15,29 @@ const getMockProps = () => ({
   },
 })
 
-// To handle testing when using withTheme
-// https://material-ui.com/guides/testing/
-let shallow
-beforeEach(() => {
-  shallow = createShallow({ dive: true })
-})
-
 describe('financials page', () => {
   it('renders without error', () => {
     const NotFoundPage = require('../404').default
-    shallow(<NotFoundPage {...getMockProps()} />)
+    shallow(<NotFoundPage {...getMockProps()} />).dive()
   })
 
   it('sets the page title using Helmet', () => {
     const NotFoundPage = require('../404').default
-    const wrapper = shallow(<NotFoundPage {...getMockProps()} />)
+    const wrapper = shallow(<NotFoundPage {...getMockProps()} />).dive()
     const elem = wrapper.find(Helmet)
     expect(elem.prop('title')).toBe('Missing page')
   })
 
   it('sets the open graph title', () => {
     const NotFoundPage = require('../404').default
-    const wrapper = shallow(<NotFoundPage {...getMockProps()} />)
+    const wrapper = shallow(<NotFoundPage {...getMockProps()} />).dive()
     const elem = wrapper.find('meta[property="og:title"]')
     expect(elem.prop('content')).toBe('Oops! No page here.')
   })
 
   it('sets the open graph description', () => {
     const NotFoundPage = require('../404').default
-    const wrapper = shallow(<NotFoundPage {...getMockProps()} />)
+    const wrapper = shallow(<NotFoundPage {...getMockProps()} />).dive()
     const elem = wrapper.find('meta[property="og:description"]')
     expect(elem.prop('content')).toBe('This page seems to be missing.')
   })
