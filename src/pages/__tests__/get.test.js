26d00b2a6a735c940df4e8551ced55fb3c38e340
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-22 17:40:31 -0700

    Add some failing tests

--- src/pages/__tests__/get.test.js
+++ src/pages/__tests__/get.test.js
@@ -7,9 +7,12 @@ import {
   homeURL,
   firefoxExtensionURL,
 } from 'src/utils/navigation'
+import localStorageMgr from 'src/utils/local-storage'
+import { STORAGE_REFERRAL_DATA_REFERRING_CHANNEL } from 'src/utils/constants'
 
 jest.mock('browser-detect')
 jest.mock('src/utils/redirect')
+jest.mock('src/utils/local-storage')
 
 const createMockBrowserInfo = (browser = 'chrome', mobile = false) => {
   return {
@@ -144,5 +147,44 @@ describe('GetExtensionRedirectPage', () => {
     const mockProps = getMockProps()
     shallow(<GetExtensionRedirectPage {...mockProps} />)
     expect(redirect).toHaveBeenCalledWith(homeURL)
+  })
+
+  it('stores the referrer ID in local storage if it exists', () => {
+    expect.assertions(1)
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('chrome', false))
+
+    const GetExtensionRedirectPage = require('../get').default
+    const mockProps = getMockProps()
+    mockProps.location.search = '?src=hi&r=1357&foo'
+    shallow(<GetExtensionRedirectPage {...mockProps} />)
+    expect(localStorageMgr.setItem).toHaveBeenCalledWith(
+      STORAGE_REFERRAL_DATA_REFERRING_CHANNEL,
+      1357
+    )
+  })
+
+  it('does not call local storage if there is no referrer ID', () => {
+    expect.assertions(1)
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('chrome', false))
+
+    const GetExtensionRedirectPage = require('../get').default
+    const mockProps = getMockProps()
+    mockProps.location.search = '?foo=bar'
+    shallow(<GetExtensionRedirectPage {...mockProps} />)
+    expect(localStorageMgr.setItem).not.toHaveBeenCalled()
+  })
+
+  it('does not call local storage if the referrer ID is not a number', () => {
+    expect.assertions(1)
+    const detectBrowser = require('browser-detect').default
+    detectBrowser.mockReturnValueOnce(createMockBrowserInfo('chrome', false))
+
+    const GetExtensionRedirectPage = require('../get').default
+    const mockProps = getMockProps()
+    mockProps.location.search = '?r=hello'
+    shallow(<GetExtensionRedirectPage {...mockProps} />)
+    expect(localStorageMgr.setItem).not.toHaveBeenCalled()
   })
 })
