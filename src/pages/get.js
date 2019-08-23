63d0c9f1aea3b7279d8e53577f07ab08708ce522
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-22 17:51:34 -0700

    Save any referrer to local storage

--- src/pages/get.js
+++ src/pages/get.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import detectBrowser from 'browser-detect'
+import qs from 'qs'
 import {
   CHROME_BROWSER,
   FIREFOX_BROWSER,
@@ -12,14 +13,27 @@ import {
   firefoxExtensionURL,
 } from 'src/utils/navigation'
 import redirect from 'src/utils/redirect'
+import localStorageMgr from 'src/utils/local-storage'
+import { STORAGE_REFERRAL_DATA_REFERRING_CHANNEL } from 'src/utils/constants'
 
 class GetExtensionRedirectPage extends React.Component {
   componentDidMount() {
-    // TODO: store referral parameter
-    // const { location } = this.props
-    // console.log(location)
-
     try {
+      // If there is a referrer, save it to local storage.
+      const { location: { search = '' } = {} } = this.props
+      const queryParams = qs.parse(search, { ignoreQueryPrefix: true })
+      const referrerId =
+        queryParams.r && !isNaN(parseInt(queryParams.r, 10))
+          ? parseInt(queryParams.r, 10)
+          : null
+      if (referrerId) {
+        localStorageMgr.setItem(
+          STORAGE_REFERRAL_DATA_REFERRING_CHANNEL,
+          referrerId
+        )
+      }
+
+      // Based on the browser, go to the appropriate extension page.
       const browser = this.detectBrowser()
       switch (browser) {
         case CHROME_BROWSER:
