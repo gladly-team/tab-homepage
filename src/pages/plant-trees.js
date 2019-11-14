210935052d05bba81d0248afa4522e2bb04796bd
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-14 11:20:19 -0800

    Add text

--- src/pages/plant-trees.js
+++ src/pages/plant-trees.js
@@ -5,9 +5,133 @@ import TextPageContent from 'src/components/TextPageContent'
 import TextPageHeader from 'src/components/TextPageHeader'
 import Layout from 'src/components/Layout'
 
+const faqs = [
+  {
+    question: 'How do I plant a tree?',
+    answerElem: (
+      <div>
+        <p>
+          We'll plant a tree for every person who joins Tab for a Cause—so join
+          on in!
+        </p>
+        <p>
+          If you're already using Tab for a Cause, refer a friend using your
+          referral URL, and we'll plant another tree.
+        </p>
+      </div>
+    ),
+  },
+  {
+    question: "Wait, it's free?",
+    answerElem: (
+      <p>
+        Yes! Tab for a Cause is a free way for you to do good every single day.
+        You just have to open tabs in your browser—and, let's face it, we all
+        have a few too many tabs open.
+      </p>
+    ),
+  },
+  {
+    question: 'How are the trees actually planted?',
+    answerElem: (
+      <p>
+        We are working with the{' '}
+        <a href={'https://edenprojects.org/'}>Eden Reforestation Project</a> to
+        plant trees around the world. They work with local communities in
+        Madagascar, Haiti, Nepal, Mozambique, and Indonesia to plant, protect,
+        and care for native tree species.
+      </p>
+    ),
+  },
+  {
+    question: 'Why trees?',
+    answerElem: (
+      <p>
+        Trees are an incredibly effective way to address habit loss, erosion,
+        and CO2.{' '}
+        <a href={'https://edenprojects.org/the-problem-and-the-solution/'}>
+          Read what Eden Reforestation Project says about their work.
+        </a>
+      </p>
+    ),
+  },
+  {
+    question: 'Why now?',
+    answerElem: (
+      <p>
+        Historically, Tab for a Cause has grown through a combination of
+        word-of-mouth and paid marketing. However, during November and December
+        each year, the cost of paid marketing increases significantly. Instead
+        of paying for expensive ads, with your help we can turn our marketing
+        budget into thousands of trees. Thanks for spreading the word!
+      </p>
+    ),
+  },
+  {
+    question: 'Why should I spread the word about Tab for a Cause?',
+    answerElem: (
+      <div>
+        <p>
+          Of course, there are lots of reasons to tell your friends about Tab
+          for a Cause, but here are some you might not know:
+        </p>
+        <ol>
+          <li>
+            As we increase the amount of money we are donating to charities, we
+            are able to support more unique projects like launching this
+            partnership with Eden Project or{' '}
+            <a
+              href={
+                'https://www.roomtoread.org/the-latest/guest-blogger-how-opening-tabs-for-a-cause-launched-a-library-in-vietnam/'
+              }
+            >
+              building a school library through Room to Read
+            </a>
+            .
+          </li>
+          <li>
+            Advertisers care about the size of the audience they can reach. With
+            more Tabbers, we earn more money on each individual ad, meaning the
+            value of each individual tab grows as we grow!
+          </li>
+          <li>Why not? :)</li>
+          <li>
+            People are much more likely to use something that they heard about
+            from someone they know.
+          </li>
+        </ol>
+      </div>
+    ),
+  },
+  {
+    question: 'Can I just create a bunch of new accounts to plant trees?',
+    answerElem: (
+      <p>
+        No, we will only be planting trees for legitimate accounts that are
+        being actively used. The spirit of this project is to help spread the
+        word so we can raise more money for nonprofits, and having multiple
+        accounts doesn't help with that goal.
+      </p>
+    ),
+  },
+  {
+    question: 'Do bananas grow on trees?',
+    answerElem: (
+      <p>
+        Great question! In fact,{' '}
+        <a href={'https://en.wikipedia.org/wiki/Banana#Description'}>
+          banana plants are <span style={{ fontWeight: 'bold' }}>not</span>{' '}
+          trees.
+        </a>{' '}
+        And—wait for it—bananas are botanically considered a berry :o
+      </p>
+    ),
+  },
+]
+
 const PlantTreesPage = props => {
   const { location } = props
-  const openGraphTitle = 'Plant Trees with Tab for a Cause'
+  const openGraphTitle = 'Plant Trees for Free'
   const openGraphDescription =
     'We are planting a tree for every person who joins Tab for a Cause from now until January 10, 2020.'
   return (
@@ -19,9 +143,50 @@ const PlantTreesPage = props => {
           <meta name="twitter:title" content={openGraphTitle} />
           <meta name="twitter:description" content={openGraphDescription} />
         </Helmet>
-        <TextPageHeader>Plant Trees This Holiday Season!</TextPageHeader>
-        <div style={{ marginBottom: 50 }}>
-          <p>Lorem ipsum</p>
+        <TextPageHeader>Plant Trees for Free!</TextPageHeader>
+        <div style={{ padding: '12px 0px' }}>
+          <p>
+            Tab for a Cause is already one of the easiest ways to do good every
+            day, for free. Now, your new tab page is getting a little greener!
+          </p>
+          <p style={{ fontWeight: 'bold' }}>
+            Tab for a Cause has partnered with Eden Reforestation Projects to
+            plant a tree for every person that signs up for Tab for a Cause this
+            holiday season!
+          </p>
+          <p>
+            Trees are amazing plants that fight climate change through carbon
+            sequestration, guard land against erosion, provide habitat for
+            countless organisms, and provide economic resources for surrounding
+            communities. Unfortunately, deforestation has wiped out tree
+            populations across the globe, so we need to reinvest in one of
+            nature's amazing organisms and replant our forests.
+          </p>
+          <p>
+            To do our part,{' '}
+            <span style={{ fontWeight: 'bold' }}>
+              Tab for a Cause will plant a tree for each new Tabber from
+              November 14, 2019 to January 10, 2020
+            </span>
+            . If you are a current Tabber, you can plant a tree right now by
+            getting one friend or family member to start Tabbing.
+          </p>
+          <p>
+            It's incredibly simple to turn your tabs into support for some
+            amazing nonprofits. Now is the perfect time to get your friends to
+            join you in turning internet browsing into a charitable act.
+          </p>
+        </div>
+        <div style={{ padding: '12px 0px' }}>
+          <h2>FAQ</h2>
+          {faqs.map(faq => {
+            return (
+              <div key={faq.question} style={{ padding: '10px 0px' }}>
+                <p style={{ fontWeight: 'bold' }}>{faq.question}</p>
+                {faq.answerElem}
+              </div>
+            )
+          })}
         </div>
       </TextPageContent>
     </Layout>
