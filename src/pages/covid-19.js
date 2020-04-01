69e5a0a6690da6a4dacec1d9c8271d80cb15ed59
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-01 11:29:56 -0700

    Styling and FAQ

--- src/pages/covid-19.js
+++ src/pages/covid-19.js
@@ -31,17 +31,17 @@ const installButtonBlock = (
 
 const faqs = [
   {
-    question: 'How do I plant a tree?',
+    question: 'How can I help the COVID-19 response efforts?',
     answerElem: (
       <div>
         <p style={{ marginBottom: 0 }}>
-          We'll plant a tree for every person who joins Tab for a Cause—so join
-          on in!
+          Join Tab for a Cause! After it's added, every tab you open during
+          these campaigns will support COVID-19 relief efforts.
         </p>
         {installButtonBlock}
         <p>
-          If you're already using Tab for a Cause, refer a friend using your
-          referral URL, and we'll plant another tree.
+          If you're already using Tab for a Cause, get your friends on board to
+          increase our community's impact.
         </p>
       </div>
     ),
@@ -51,105 +51,25 @@ const faqs = [
     answerElem: (
       <p>
         Yes! Tab for a Cause is a free way for you to do good every single day.
-        You just have to open tabs in your browser—and, let's face it, we all
-        have a few too many tabs open.
-      </p>
-    ),
-  },
-  {
-    question: 'How are the trees actually planted?',
-    answerElem: (
-      <p>
-        We are working with the{' '}
-        <a href={'https://edenprojects.org/'}>Eden Reforestation Project</a> to
-        plant trees around the world. They work with local communities in
-        Madagascar, Haiti, Nepal, Mozambique, and Indonesia to plant, protect,
-        and care for native tree species.
-      </p>
-    ),
-  },
-  {
-    question: 'Why trees?',
-    answerElem: (
-      <p>
-        Trees are an incredibly effective way to address habit loss, erosion,
-        and CO2.{' '}
-        <a href={'https://edenprojects.org/the-problem-and-the-solution/'}>
-          Read what Eden Reforestation Project says about their work.
-        </a>
+        You just have to open tabs in your browser, and we send money raised
+        from ads to some spectacular nonprofits.
       </p>
     ),
   },
   {
     question: 'Why now?',
     answerElem: (
-      <p>
-        Historically, Tab for a Cause has grown through a combination of
-        word-of-mouth and paid marketing. However, during November and December
-        each year, the cost of paid marketing increases significantly. Instead
-        of paying for expensive ads, with your help we can turn our marketing
-        budget into thousands of trees. Thanks for spreading the word!
-      </p>
-    ),
-  },
-  {
-    question: 'Why should I spread the word about Tab for a Cause?',
-    answerElem: (
       <div>
         <p>
-          Of course, there are lots of reasons to tell your friends about Tab
-          for a Cause, but here are some you might not know:
+          The world is facing an unprecedented challenge in the spread of
+          coronavirus, and it needs an unprecedented response.
         </p>
-        <ol>
-          <li>
-            As we increase the amount of money we are donating to charities, we
-            are able to support more unique projects like launching this
-            partnership with Eden Project or{' '}
-            <a
-              href={
-                'https://www.roomtoread.org/the-latest/guest-blogger-how-opening-tabs-for-a-cause-launched-a-library-in-vietnam/'
-              }
-            >
-              building a school library through Room to Read
-            </a>
-            .
-          </li>
-          <li>
-            Advertisers care about the size of the audience they can reach. With
-            more Tabbers, we earn more money on each individual ad, meaning the
-            value of each individual tab grows as we grow!
-          </li>
-          <li>Why not? :)</li>
-          <li>
-            People are much more likely to use something that they heard about
-            from someone they know.
-          </li>
-        </ol>
+        <p>
+          As a way to raise money for free from the safety of our homes, Tab for
+          a Cause is a great way to support communities in need as we confront
+          this pandemic.
+        </p>
       </div>
-    ),
-  },
-  {
-    question: 'Can I just create a bunch of new accounts to plant trees?',
-    answerElem: (
-      <p>
-        No, we will only be planting trees for legitimate accounts that are
-        being actively used. The spirit of this project is to help spread the
-        word so we can raise more money for nonprofits, and having multiple
-        accounts doesn't help with that goal.
-      </p>
-    ),
-  },
-  {
-    question: 'Do bananas grow on trees?',
-    answerElem: (
-      <p>
-        Great question! In fact,{' '}
-        <a href={'https://en.wikipedia.org/wiki/Banana#Description'}>
-          banana plants are <span style={{ fontWeight: 'bold' }}>not</span>{' '}
-          trees.
-        </a>{' '}
-        And—wait for it—bananas are botanically considered a berry :o
-      </p>
     ),
   },
 ]
@@ -187,8 +107,8 @@ const Covid19 = props => {
           >
             <p>
               The spread of COVID-19 has been swift and destructive. We need a
-              global response to support the health systems working to keep us
-              all safe.
+              global response to support the health systems and community
+              organizations working to keep us all safe.
             </p>
             <p
               style={{
@@ -196,8 +116,8 @@ const Covid19 = props => {
               }}
             >
               Tab for a Cause is a free, simple, and at-home way to raise money
-              for important causes, and we are raising funds for critical
-              response efforts.
+              for important causes, and we are raising funds for critical relief
+              efforts.
             </p>
             {installButtonBlock}
             <p>
@@ -263,62 +183,70 @@ const Covid19 = props => {
               style={{ width: '100%', maxWidth: 1000 }}
             />
           </div>
-        </div>
-
-        <div
-          style={{
-            marginTop: 40,
-            marginBottom: 40,
-          }}
-        >
-          <h5 style={campaignLabelStyle}>Campaign #2: April 1 – ongoing</h5>
-
-          <h2 style={{ fontSize: '1.8rem' }}>
-            Helping to Feed Families in Need
-          </h2>
-          <p>
-            COVID-19 has strained the ability of food banks to take care of
-            people in need. This phase of our support helps the{' '}
-            <a href={'https://www.foodbanknyc.org/covid-19/'}>
-              Food Bank for New York City
-            </a>{' '}
-            keep families fed during this crisis.
-          </p>
-          <p>
-            Right now, tabs you open are providing meals for our fellow humans
-            in NYC. Our goal is to provide over 10,000 meals in the next few
-            days.
-          </p>
-          <p>
-            Together, we can feed thousands of people in need—so please open a
-            few tabs to help out!
-          </p>
-          {installButtonBlock}
-        </div>
-        <div
-          style={{
-            marginTop: 40,
-            marginBottom: 40,
-            // Full page width CSS
-            display: 'flex',
-            justifyContent: 'center',
-            width: '100vw',
-            position: 'relative',
-            left: '50%',
-            right: '50%',
-            marginLeft: '-50vw',
-            marginRight: '-50vw',
-            // End full page width CSS
-          }}
-        >
-          <img
-            alt={"Information on the World Health Organization's impact"}
-            src={foodBankNYCPhoto}
-            style={{ width: '100%', maxWidth: 1000 }}
+          <Divider
+            style={{ backgroundColor: lightestTextColor, marginTop: 30 }}
           />
         </div>
 
-        <div style={{ padding: '12px 0px' }}>
+        <div>
+          <div
+            style={{
+              marginTop: 40,
+              marginBottom: 40,
+            }}
+          >
+            <h5 style={campaignLabelStyle}>Campaign #2: April 1 – ongoing</h5>
+
+            <h2 style={{ fontSize: '1.8rem' }}>
+              Helping to Feed Families in Need
+            </h2>
+            <p>
+              COVID-19 has strained the ability of food banks to take care of
+              people in need. This phase of our support helps the{' '}
+              <a href={'https://www.foodbanknyc.org/covid-19/'}>
+                Food Bank for New York City
+              </a>{' '}
+              keep families fed during this crisis.
+            </p>
+            <p>
+              Right now, tabs you open are providing meals for our fellow humans
+              in NYC. Our goal is to provide over 10,000 meals in the next few
+              days.
+            </p>
+            <p>
+              Together, we can feed thousands of people in need—so please open a
+              few tabs to help out.
+            </p>
+            {installButtonBlock}
+          </div>
+          <div
+            style={{
+              marginTop: 40,
+              marginBottom: 40,
+              // Full page width CSS
+              display: 'flex',
+              justifyContent: 'center',
+              width: '100vw',
+              position: 'relative',
+              left: '50%',
+              right: '50%',
+              marginLeft: '-50vw',
+              marginRight: '-50vw',
+              // End full page width CSS
+            }}
+          >
+            <img
+              alt={"Information on the World Health Organization's impact"}
+              src={foodBankNYCPhoto}
+              style={{ width: '100%', maxWidth: 1000 }}
+            />
+          </div>
+          <Divider
+            style={{ backgroundColor: lightestTextColor, marginTop: 30 }}
+          />
+        </div>
+
+        <div style={{ marginTop: 40, padding: '12px 0px' }}>
           <h2>FAQ</h2>
           {faqs.map(faq => {
             return (
