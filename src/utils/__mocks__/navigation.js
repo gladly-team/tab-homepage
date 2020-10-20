ba5ef57fa01b7ca467ce2da11823cf886b8d02ef
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-20 12:19:25 -0700

    Fix tests

--- src/utils/__mocks__/navigation.js
+++ src/utils/__mocks__/navigation.js
@@ -2,7 +2,7 @@
 
 // Most exported values are strings, so just mock
 // what we need to change.
-const navigation = require.requireActual('../navigation')
+const navigation = jest.requireActual('../navigation')
 
 navigation.getAbsoluteURL = jest.fn((path) => `https://example.com${path}`)
 
