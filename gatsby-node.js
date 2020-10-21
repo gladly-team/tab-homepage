842171e192c339a744756fe4ee96a5d8214540fb
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-21 14:40:36 -0700

    Remove useless catch

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,35 +15,31 @@ exports.createPages = async ({ actions, graphql }) => {
 
   // Create landing page variants for referrers.
   const homepage = path.resolve(`src/pages/index.js`)
-  try {
-    const response = await graphql(`
-      {
-        allReferrersYaml(limit: 5000) {
-          edges {
-            node {
-              path
-              referrerId
-            }
+  const response = await graphql(`
+    {
+      allReferrersYaml(limit: 5000) {
+        edges {
+          node {
+            path
+            referrerId
           }
         }
       }
-    `)
-    response.data.allReferrersYaml.edges.forEach(({ node }) => {
-      // Not all referrers will have a vanity URL.
-      if (!node.path || !node.referrerId) {
-        return
-      }
-      createPage({
-        path: `${node.path}/`,
-        component: homepage,
-        context: {
-          referrer: {
-            id: node.referrerId,
-          },
+    }
+  `)
+  response.data.allReferrersYaml.edges.forEach(({ node }) => {
+    // Not all referrers will have a vanity URL.
+    if (!node.path || !node.referrerId) {
+      return
+    }
+    createPage({
+      path: `${node.path}/`,
+      component: homepage,
+      context: {
+        referrer: {
+          id: node.referrerId,
         },
-      })
+      },
     })
-  } catch (e) {
-    throw e
-  }
+  })
 }
