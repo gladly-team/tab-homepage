f6f6043a180c8b58b5797f457ee4467865d0db2d
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-21 15:07:29 -0700

    Add react-fullpage

--- src/pages/million.js
+++ src/pages/million.js
@@ -1,36 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import Layout from 'src/components/Layout'
+import ReactFullpage from '@fullpage/react-fullpage'
+import { ThemeProvider } from '@material-ui/styles'
+import defaultTheme from 'src/themes/theme'
 import InstallButton from 'src/components/InstallButton'
 import redirect from 'src/utils/redirect'
 import { homeURL } from 'src/utils/navigation'
 
-const MillionPage = (props) => {
-  const { location } = props
-
+const MillionPage = () => {
   // TODO
   const openGraphTitle = 'Million raised'
   const openGraphDescription = 'We raised a million!'
 
   return (
-    <Layout brand={'tab'} location={location}>
+    <ThemeProvider theme={defaultTheme}>
       <Helmet title={openGraphTitle}>
         <meta property="og:title" content={openGraphTitle} />
         <meta property="og:description" content={openGraphDescription} />
         <meta name="twitter:title" content={openGraphTitle} />
         <meta name="twitter:description" content={openGraphDescription} />
       </Helmet>
-      <p>hi</p>
-      <div>
-        <InstallButton
-          size={'medium'}
-          onUnsupportedBrowserInstallClick={() => {
-            redirect(homeURL)
-          }}
-        />
-      </div>
-    </Layout>
+      <ReactFullpage
+        //fullpage options
+        licenseKey={'YOUR_KEY_HERE'}
+        scrollingSpeed={1000} /* Options here */
+        render={({ _, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <p>Section 1 (welcome to fullpage.js)</p>
+                <button onClick={() => fullpageApi.moveSectionDown()}>
+                  Click me to move down
+                </button>
+                <p>hi</p>
+                <div>
+                  <InstallButton
+                    size={'medium'}
+                    onUnsupportedBrowserInstallClick={() => {
+                      redirect(homeURL)
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="section">
+                <p>Section 2</p>
+              </div>
+            </ReactFullpage.Wrapper>
+          )
+        }}
+      />
+    </ThemeProvider>
   )
 }
 
