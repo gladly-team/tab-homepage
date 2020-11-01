eb2403b3e1f51c9fb1249b86482e4ee1452bfff3
Kevin Jennison <kevin.jennison1@gmail.com>
2020-11-01 12:47:47 -0800

    Use es6 module for gatsby-node

---
+++ gatsby-node.esm.js
@@ -0,0 +1,55 @@
+/* globals exports */
+
+import path from 'path'
+
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// Implement the Gatsby API "createPages". This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  // Create landing page variants for referrers.
+  const homepage = path.resolve(`src/pages/index.js`)
+  const response = await graphql(`
+    {
+      allReferrersYaml(limit: 5000) {
+        edges {
+          node {
+            path
+            referrerId
+          }
+        }
+      }
+    }
+  `)
+  response.data.allReferrersYaml.edges.forEach(({ node }) => {
+    // Not all referrers will have a vanity URL.
+    if (!node.path || !node.referrerId) {
+      return
+    }
+    createPage({
+      path: `${node.path}/`,
+      component: homepage,
+      context: {
+        referrer: {
+          id: node.referrerId,
+        },
+      },
+    })
+  })
+}
+
+exports.onCreatePage = async ({ page, actions: { deletePage } }) => {
+  // Only conditionally build the "million raised" page.
+  if (page.path.match(/^\/million/)) {
+    // eslint-disable-next-line no-undef
+    if (!(process.env.SHOW_MILLION_RAISED_PAGE === 'true')) {
+      deletePage(page)
+    }
+  }
+}
