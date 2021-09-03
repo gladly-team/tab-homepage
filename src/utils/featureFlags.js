5ef521012303aba36a9c1fdd331bb3e131bef6d9
Kevin Jennison <kevin.jennison1@gmail.com>
2021-09-03 12:34:16 -0400

    Add TODO

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -1,1 +1,4 @@
+// TODO: when setting to true:
+// * update text in UnsupportedBrowserDialog.js to include Safari
+// * set `safariExtensionURL` in navigation.js
 export const safariEnabled = () => false
