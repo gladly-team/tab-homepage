5d6ff2a08d02289f2582bb0a355689eb046db6f6
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-10-16 19:57:41 -0700

    update constant and tab e2e

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,5 +17,5 @@ export const SEARCH_STORAGE_REFERRAL_DATA_REFERRING_USER =
 export const SEARCH_STORAGE_REFERRAL_DATA_REFERRING_CHANNEL =
   'search.referralData.referringChannel'
 export const STORAGE_NEW_USER_CAUSE_ID = 'tab.newUser.causeId'
-export const STORAGE_CATS_CAUSE_ID = '6ce5ad8e-7dd4-4de5-ba4f-13868e7d212z'
+export const STORAGE_CATS_CAUSE_ID = '7f8476b9-f83f-47ac-8173-4a1c2ec3dc29'
 export const STORAGE_SEAS_CAUSE_ID = 'c04b544f-5e6b-4e10-b783-05834e5d087d'
