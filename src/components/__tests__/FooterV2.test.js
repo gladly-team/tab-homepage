d1d4f46fed2c788698d42922f5f16e9c7e68be82
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-11-09 16:12:57 -0800

    remove snapshot

--- src/components/__tests__/FooterV2.test.js
+++ src/components/__tests__/FooterV2.test.js
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { shallow, mount } from 'enzyme'
-import toJson from 'enzyme-to-json'
 import GoogleChrome from 'mdi-material-ui/GoogleChrome'
 import MicrosoftEdge from 'mdi-material-ui/MicrosoftEdge'
 import AppleSafari from 'mdi-material-ui/AppleSafari'
@@ -12,12 +11,6 @@ describe('FooterV2', () => {
   it('renders without error', () => {
     const FooterV2 = require('../FooterV2').default
     shallow(<FooterV2 {...mockProps} />)
-  })
-
-  it('matches expected snapshot', function () {
-    const FooterV2 = require('../FooterV2').default
-    const wrapper = shallow(<FooterV2 {...mockProps} />)
-    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   it('clicking on the chrome icon sets local storage info', () => {
