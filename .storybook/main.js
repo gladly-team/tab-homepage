e51cf74f5f5db853dd56e42f031302fec23099da
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-12-06 14:31:17 -0800

    adjust path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  staticDirs: [{ from: '../src/data/img', to: '/static/media/src/' }],
+  // include images in deployment and mimick s3 filepath
+  staticDirs: [{ from: '../src/img', to: '/static/media/src/' }],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
