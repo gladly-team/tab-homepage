6b87bd67ca1dc35a57021d79517bc708eab68dc5
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-12-06 14:47:54 -0800

    see where these images are added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   // include images in deployment and mimick s3 filepath
-  staticDirs: [{ from: '../src/img', to: '/static/media/src/' }],
+  staticDirs: ['../src/img'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
