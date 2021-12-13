c3726e93a3e5441cb2495a68f094da628523cece
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-12-13 09:23:32 -0800

    fix data

--- src/components/V4HomePage.stories.jsx
+++ src/components/V4HomePage.stories.jsx
@@ -3,6 +3,58 @@ import React from 'react'
 import HomePageWrapper from './V4HomePage'
 import catsData from 'src/data/causes/cats.json'
 import seasData from 'src/data/causes/seas.json'
+seasData.data.sections.Financials.pdfs = [
+  {
+    quarter: 1,
+    year: 2021,
+    pdfUrl: '/financials/2021-Q1.pdf',
+    img: seasData.data.sections.Financials.q1Img,
+  },
+  {
+    quarter: 2,
+    year: 2021,
+    pdfUrl: '/financials/2021-Q2.pdf',
+    img: seasData.data.sections.Financials.q2Img,
+  },
+  {
+    quarter: 4,
+    year: 2020,
+    pdfUrl: '/financials/2020-Q4.pdf',
+    img: seasData.data.sections.Financials.q4Img,
+  },
+  {
+    quarter: 3,
+    year: 2020,
+    pdfUrl: '/financials/2020-Q3.pdf',
+    img: seasData.data.sections.Financials.q3Img,
+  },
+]
+catsData.data.sections.Financials.pdfs = [
+  {
+    quarter: 1,
+    year: 2021,
+    pdfUrl: '/financials/2021-Q1.pdf',
+    img: catsData.data.sections.Financials.q1Img,
+  },
+  {
+    quarter: 2,
+    year: 2021,
+    pdfUrl: '/financials/2021-Q2.pdf',
+    img: catsData.data.sections.Financials.q2Img,
+  },
+  {
+    quarter: 4,
+    year: 2020,
+    pdfUrl: '/financials/2020-Q4.pdf',
+    img: catsData.data.sections.Financials.q4Img,
+  },
+  {
+    quarter: 3,
+    year: 2020,
+    pdfUrl: '/financials/2020-Q3.pdf',
+    img: catsData.data.sections.Financials.q3Img,
+  },
+]
 // enable after footer pr
 // import { mobile, seas, cats } from '../../.storybook/boilerPlate'
 export default {
