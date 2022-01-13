f9a7ea6aa7d59871ac450903cd2d139c02b66934
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-13 14:46:22 -0500

    Align step text to left

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -93,7 +93,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     direction: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     padding: theme.spacing(1),
     [theme.breakpoints.down('sm')]: {
       minHeight: 'unset',
