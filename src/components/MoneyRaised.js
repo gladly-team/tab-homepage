70c46cb71a1b4ee6df1ebb21042d240c8cba28de
jedtan <jedtan@gmail.com>
2022-01-20 12:25:31 -0500

    Stuff

--- src/components/MoneyRaised.js
+++ src/components/MoneyRaised.js
@@ -1,86 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
 import { commaFormatted, currencyFormatted } from 'src/utils/formatting'
-import { isChromaticEnv } from '../utils/featureFlags'
+import useMoneyRaised  from 'src/hooks/useMoneyRaised'
 
-// These must match the new tab page's numbers.
-// Eventually call the API instead of hard-coding.
-const MONEY_RAISED = 1057100.0
-const MONEY_RAISED_UPDATE_TIME = moment('2021-01-07T16:30:00.000Z')
-const DOLLARS_PER_DAY_RATE = 700.0
-
-class MoneyRaised extends React.Component {
-  constructor(props) {
-    super(props)
-    this.timer = 0
-    this.state = {
-      moneyRaised: 0,
-    }
-  }
-
-  componentDidMount() {
-    this.setCounter()
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.timer)
-  }
-
-  calculateMoneyRaised() {
-    const secsInDay = 60 * 60 * 24
-    const now = moment()
-    const diff = now.diff(MONEY_RAISED_UPDATE_TIME, 'seconds')
-    const daysDiff = diff / secsInDay
-    if (isChromaticEnv()) {
-      return MONEY_RAISED
-    }
-    return MONEY_RAISED + daysDiff * DOLLARS_PER_DAY_RATE
-  }
-
-  incrementAmount() {
-    var amountRaised = this.state.moneyRaised
-    var newAmountRaised = amountRaised + 0.01
-    this.setState({
-      moneyRaised: newAmountRaised,
-    })
-  }
-
-  setCounter() {
-    const secondsInDay = 60 * 60 * 24
-    // Recalculate based on time that elapsed since the base amount.
-    const secondsPerPenny = secondsInDay / DOLLARS_PER_DAY_RATE / 100
-    this.setState(
-      {
-        moneyRaised: this.calculateMoneyRaised(),
-      },
-      () => {
-        this.props.onLoaded()
-      }
-    )
-
-    // Set an interval to add a penny to the money raised.
-    if (!(secondsPerPenny <= 0)) {
-      var millisecondsPerPenny = Math.round(Math.abs(secondsPerPenny) * 1000)
-      if (!isChromaticEnv()) {
-        this.timer = window.setInterval(
-          this.incrementAmount.bind(this),
-          millisecondsPerPenny
-        )
-      }
-    }
-  }
-
-  render() {
-    if (!this.state.moneyRaised) {
-      return null
-    }
-    const moneyRaised = this.state.moneyRaised
-    var moneyRaisedFormatted = `$${commaFormatted(
-      currencyFormatted(moneyRaised)
-    )}`
-    return <span className={this.props.className}>{moneyRaisedFormatted}</span>
-  }
+const MoneyRaised = () => {
+  const moneyRaised = useMoneyRaised()
+  var moneyRaisedFormatted = `$${commaFormatted(
+    currencyFormatted(moneyRaised)
+  )}`
+  return moneyRaised ? (
+    <span className={this.props.className}>{moneyRaisedFormatted}</span>
+  ) : null
 }
 
 MoneyRaised.propTypes = {
---
+++ src/hooks/useMoneyRaised.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import moment from 'moment'
+import { isChromaticEnv } from '../utils/featureFlags'
+
+// These must match the new tab page's numbers.
+// Eventually call the API instead of hard-coding.
+const MONEY_RAISED = 1057100.0
+const MONEY_RAISED_UPDATE_TIME = moment('2021-01-07T16:30:00.000Z')
+const DOLLARS_PER_DAY_RATE = 700.0
+
+const useMoneyRaised = () => {
+  const [moneyRaised, setMoneyRaised] = useState(0)
+  const [timer, setTimer] = useState(0)
+
+  const calculateMoneyRaised = () => {
+    const secsInDay = 60 * 60 * 24
+    const now = moment()
+    const diff = now.diff(MONEY_RAISED_UPDATE_TIME, 'seconds')
+    const daysDiff = diff / secsInDay
+    if (isChromaticEnv()) {
+      return MONEY_RAISED
+    }
+    return MONEY_RAISED + daysDiff * DOLLARS_PER_DAY_RATE
+  }
+
+  const incrementAmount = () => {
+    var amountRaised = moneyRaised
+    var newAmountRaised = amountRaised + 0.01
+    setMoneyRaised(newAmountRaised)
+  }
+
+  const setCounter = () => {
+    const secondsInDay = 60 * 60 * 24
+    // Recalculate based on time that elapsed since the base amount.
+    const secondsPerPenny = secondsInDay / DOLLARS_PER_DAY_RATE / 100
+    setMoneyRaised(calculateMoneyRaised())
+
+    // Set an interval to add a penny to the money raised.
+    if (!(secondsPerPenny <= 0)) {
+      var millisecondsPerPenny = Math.round(Math.abs(secondsPerPenny) * 1000)
+      if (!isChromaticEnv()) {
+        setTimer(
+          window.setInterval(incrementAmount.bind(this), millisecondsPerPenny)
+        )
+      }
+    }
+  }
+
+  useEffect(() => {
+    setCounter()
+    return function cleanup() {
+      window.clearInterval(timer)
+    }
+  })
+
+  return [moneyRaised]
+}
+
+export default useMoneyRaised
