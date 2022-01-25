bf2806b6e06d09c1248b8de7254997a9632452bd
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-25 16:34:50 -0500

    Remove adaptV4Theme

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,4 +1,4 @@
-import { createTheme, adaptV4Theme } from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles'
 
 // Text on white background
 export const textColor = 'rgba(33, 33, 33, 0.82)'
@@ -28,95 +28,160 @@ export const secondaryMainColor = '#4a90e2'
 export const secondaryContrastTextColor = '#fff'
 export const baseFontSize = 16
 
-const theme = createTheme(
-  adaptV4Theme({
-    body: {
-      padding: '0px',
+const theme = createTheme({
+  body: {
+    padding: '0px',
+  },
+  palette: {
+    background: {
+      // This value changes the HTML background color:
+      // https://material-ui.com/api/css-baseline/
+      default: '#fff',
     },
-    palette: {
-      background: {
-        // This value changes the HTML background color:
-        // https://material-ui.com/api/css-baseline/
-        default: '#fff',
-      },
-      primary: {
-        // light: will be calculated from palette.primary.main,
-        main: primaryMainColor,
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: primaryMainColor,
 
-        // dark: will be calculated from palette.primary.main,
-        contrastText: primaryContrastTextColor,
-      },
-      text: {
-        secondary: '#fff',
-      },
-      secondary: {
-        // light: will be calculated from palette.primary.main,
-        main: secondaryMainColor,
+      // dark: will be calculated from palette.primary.main,
+      contrastText: primaryContrastTextColor,
+    },
+    text: {
+      secondary: '#fff',
+    },
+    secondary: {
+      // light: will be calculated from palette.primary.main,
+      main: secondaryMainColor,
 
-        // dark: will be calculated from palette.primary.main,
-        contrastText: secondaryContrastTextColor,
+      // dark: will be calculated from palette.primary.main,
+      contrastText: secondaryContrastTextColor,
+    },
+  },
+  shape: {
+    borderRadius: 2,
+  },
+  typography: {
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    fontSize: baseFontSize,
+    useNextVariants: true,
+    subtitle2: {
+      fontWeight: 700,
+    },
+    subtitle3: {
+      fontWeight: 500,
+      fontFamily: 'Poppins',
+      fontStyle: 'normal',
+      lineHeight: 1,
+      color: '#2E282A',
+    },
+  },
+  overrides: {
+    MuiButton: {
+      // Name of the styleSheet
+      root: {
+        fontWeight: '500',
       },
     },
-    shape: {
-      borderRadius: 2,
-    },
-    typography: {
-      fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
-      fontSize: baseFontSize,
-      useNextVariants: true,
-      subtitle2: {
-        fontWeight: 700,
-      },
-      subtitle3: {
-        fontWeight: 500,
-        fontFamily: 'Poppins',
-        fontStyle: 'normal',
-        lineHeight: 1,
-        color: '#2E282A',
+    MuiDivider: {
+      root: {
+        backgroundColor: lighterShadingColor,
       },
     },
-    overrides: {
-      MuiButton: {
-        // Name of the styleSheet
-        root: {
-          fontWeight: '500',
-        },
-      },
-      MuiDivider: {
-        root: {
-          backgroundColor: lighterShadingColor,
-        },
-      },
-    },
-  })
-)
+  },
+})
 
 export default theme
 
-export const tabForTeamSeasTheme = createTheme(
-  adaptV4Theme({
+export const tabForTeamSeasTheme = createTheme({
+  palette: {
+    background: {
+      // This value changes the HTML background color:
+      // https://material-ui.com/api/css-baseline/
+      default: '#FBF3E9',
+    },
+    text: {
+      secondary: '#fff',
+    },
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#5094FB',
+
+      // dark: will be calculated from palette.primary.main,
+      contrastText: primaryContrastTextColor,
+    },
+    secondary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#29BEBA',
+
+      // dark: will be calculated from palette.primary.main,
+      contrastText: secondaryContrastTextColor,
+    },
+  },
+  shape: {
+    borderRadius: 2,
+  },
+  typography: {
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    fontSize: baseFontSize,
+    useNextVariants: true,
+
+    h1: {
+      fontWeight: 900,
+      color: '#5094FB',
+      fontFamily: 'Poppins',
+      fontStyle: 'normal',
+      lineHeight: 1,
+      fontSize: '5rem',
+    },
+    h2: {
+      fontWeight: 900,
+      color: '#5094FB',
+      fontFamily: 'Poppins',
+      fontStyle: 'normal',
+      lineHeight: 1,
+      fontSize: '4rem',
+    },
+    subtitle1: {
+      fontWeight: 300,
+    },
+    subtitle2: {
+      fontWeight: 700,
+    },
+  },
+  overrides: {
+    MuiButton: {
+      // Name of the styleSheet
+      root: {
+        fontWeight: '800',
+        fontFamily: 'Poppins',
+      },
+      contained: {
+        borderRadius: 24,
+      },
+    },
+    MuiDivider: {
+      root: {
+        backgroundColor: lighterShadingColor,
+      },
+    },
+  },
+})
+
+export const createCauseTheme = ({ primary, secondary, primaryContrast }) =>
+  createTheme({
     palette: {
       background: {
-        // This value changes the HTML background color:
-        // https://material-ui.com/api/css-baseline/
         default: '#FBF3E9',
+      },
+      primary: {
+        main: primary,
+        contrastText: primaryContrast,
+      },
+      secondary: {
+        main: secondary,
+        contrastText: secondaryContrastTextColor,
       },
       text: {
         secondary: '#fff',
-      },
-      primary: {
-        // light: will be calculated from palette.primary.main,
-        main: '#5094FB',
-
-        // dark: will be calculated from palette.primary.main,
-        contrastText: primaryContrastTextColor,
-      },
-      secondary: {
-        // light: will be calculated from palette.primary.main,
-        main: '#29BEBA',
-
-        // dark: will be calculated from palette.primary.main,
-        contrastText: secondaryContrastTextColor,
       },
     },
     shape: {
@@ -129,30 +194,60 @@ export const tabForTeamSeasTheme = createTheme(
 
       h1: {
         fontWeight: 900,
-        color: '#5094FB',
+        color: primary,
         fontFamily: 'Poppins',
         fontStyle: 'normal',
-        lineHeight: 1,
+        lineHeight: 1.15,
         fontSize: '5rem',
       },
       h2: {
         fontWeight: 900,
-        color: '#5094FB',
+        color: primary,
         fontFamily: 'Poppins',
         fontStyle: 'normal',
         lineHeight: 1,
         fontSize: '4rem',
       },
+      h3: {
+        fontWeight: 800,
+        color: primary,
+        fontFamily: 'Poppins',
+        fontStyle: 'normal',
+        lineHeight: 1,
+        fontSize: '2.5rem',
+      },
       subtitle1: {
         fontWeight: 300,
+        color: '#2E282A',
+        lineHeight: 1.33,
       },
       subtitle2: {
         fontWeight: 700,
       },
+      caption: {
+        fontWeight: 500,
+        fontFamily: 'Poppins',
+        fontStyle: 'normal',
+        fontSize: '1rem',
+        lineHeight: 1.43,
+        color: '#2E282A',
+      },
+      body1: {
+        fontWeight: 400,
+        fontSize: '1.05rem',
+        fontFamily: 'Poppins',
+        lineHeight: 1.3,
+        color: '#2E282A',
+      },
+      body2: {
+        fontWeight: 300,
+        fontSize: '1rem',
+        lineHeight: 1.11,
+        color: '#2E282A',
+      },
     },
     overrides: {
       MuiButton: {
-        // Name of the styleSheet
         root: {
           fontWeight: '800',
           fontFamily: 'Poppins',
@@ -168,104 +263,3 @@ export const tabForTeamSeasTheme = createTheme(
       },
     },
   })
-)
-
-export const createCauseTheme = ({ primary, secondary, primaryContrast }) =>
-  createTheme(
-    adaptV4Theme({
-      palette: {
-        background: {
-          default: '#FBF3E9',
-        },
-        primary: {
-          main: primary,
-          contrastText: primaryContrast,
-        },
-        secondary: {
-          main: secondary,
-          contrastText: secondaryContrastTextColor,
-        },
-        text: {
-          secondary: '#fff',
-        },
-      },
-      shape: {
-        borderRadius: 2,
-      },
-      typography: {
-        fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
-        fontSize: baseFontSize,
-        useNextVariants: true,
-
-        h1: {
-          fontWeight: 900,
-          color: primary,
-          fontFamily: 'Poppins',
-          fontStyle: 'normal',
-          lineHeight: 1.15,
-          fontSize: '5rem',
-        },
-        h2: {
-          fontWeight: 900,
-          color: primary,
-          fontFamily: 'Poppins',
-          fontStyle: 'normal',
-          lineHeight: 1,
-          fontSize: '4rem',
-        },
-        h3: {
-          fontWeight: 800,
-          color: primary,
-          fontFamily: 'Poppins',
-          fontStyle: 'normal',
-          lineHeight: 1,
-          fontSize: '2.5rem',
-        },
-        subtitle1: {
-          fontWeight: 300,
-          color: '#2E282A',
-          lineHeight: 1.33,
-        },
-        subtitle2: {
-          fontWeight: 700,
-        },
-        caption: {
-          fontWeight: 500,
-          fontFamily: 'Poppins',
-          fontStyle: 'normal',
-          fontSize: '1rem',
-          lineHeight: 1.43,
-          color: '#2E282A',
-        },
-        body1: {
-          fontWeight: 400,
-          fontSize: '1.05rem',
-          fontFamily: 'Poppins',
-          lineHeight: 1.3,
-          color: '#2E282A',
-        },
-        body2: {
-          fontWeight: 300,
-          fontSize: '1rem',
-          lineHeight: 1.11,
-          color: '#2E282A',
-        },
-      },
-      overrides: {
-        MuiButton: {
-          root: {
-            fontWeight: '800',
-            fontFamily: 'Poppins',
-          },
-          contained: {
-            borderRadius: 24,
-          },
-        },
-        MuiDivider: {
-          root: {
-            backgroundColor: lighterShadingColor,
-          },
-        },
-      },
-    })
-  )
