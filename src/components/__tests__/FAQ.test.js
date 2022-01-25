cadb7f0a14203580e10a3035cfd9c20de28e98f5
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-25 15:23:31 -0500

    Fix test

--- src/components/__tests__/FAQ.test.js
+++ src/components/__tests__/FAQ.test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 
 import React from 'react'
-import { shallow } from 'src/utils/testHelpers/componentTesting'
+import { mount } from 'src/utils/testHelpers/componentTesting'
 import catsData from 'src/data/causes/cats.json'
 import Typography from '@mui/material/Typography'
 import MuiAccordion from '@mui/material/Accordion'
@@ -15,15 +15,15 @@ const getMockProps = () => ({
   faqData,
 })
 
-describe('teamseas page', () => {
+describe('FAQ', () => {
   it('renders without error', () => {
     const FAQ = require('../FAQ').default
-    shallow(<FAQ {...getMockProps()} />)
+    mount(<FAQ {...getMockProps()} />)
   })
 
   it('has the correct accordion elements', () => {
     const FAQ = require('../FAQ').default
-    const wrapper = shallow(<FAQ {...getMockProps()} />)
+    const wrapper = mount(<FAQ {...getMockProps()} />)
 
     expect(wrapper.find(MuiAccordion)).toHaveLength(faqData.questions.length)
 
@@ -33,7 +33,11 @@ describe('teamseas page', () => {
       ).toEqual(faqData.questions[i].question)
       expect(
         wrapper.find(MuiAccordionDetails).at(i).find(Markdown).children().text()
-      ).toEqual(faqData.questions[i].answer)
+      ).toEqual(
+        mount(<Markdown>{faqData.questions[i].answer}</Markdown>)
+          .render()
+          .text()
+      )
     }
   })
 })
