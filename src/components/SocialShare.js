bf4811ddac2d408d7bcdbef04cdab43a49917e69
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-27 12:15:18 -0500

    Fix warning, make SocialShare prop optional

--- src/components/SocialShare.js
+++ src/components/SocialShare.js
@@ -99,7 +99,7 @@ SocialShare.propTypes = {
     separator: PropTypes.string,
   }),
   FacebookShareButtonProps: PropTypes.shape({
-    quote: PropTypes.string.isRequired,
+    quote: PropTypes.string,
     hashtag: PropTypes.string,
   }),
   RedditShareButtonProps: PropTypes.shape({
