aafdc839451d419ede4ea862b967e50682169229
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-28 11:40:11 -0500

    Styled: Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useTheme } from '@mui/material/styles'
-import makeStyles from '@mui/styles/makeStyles'
+import { useTheme, styled } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -14,99 +13,107 @@ import Link from 'src/components/Link'
 import Wave from 'src/components/Wave'
 import V4InstallButton from 'src/components/V4InstallButton'
 
-const useStyles = makeStyles((theme) => ({
-  whiteFont: {
-    color: '#fff',
+const DivLogoContainer = styled('div')(({ theme }) => ({
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'row',
+}))
+
+const DivTitleSection = styled('div')(({ theme }) => ({
+  margin: '0 auto',
+  display: 'flex',
+
+  // for absolutely-positioned wave
+  position: 'relative',
+
+  minHeight: 'calc(100vh - 64px)',
+  alignItems: 'center',
+  paddingTop: theme.spacing(10),
+  paddingBottom: theme.spacing(10),
+
+  [theme.breakpoints.down('md')]: {
+    flexDirection: 'column-reverse',
+    height: 'auto',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
   },
-  logoContainer: { flex: 1, display: 'flex', flexDirection: 'row' },
-  titleSection: {
-    margin: '0 auto',
-    display: 'flex',
-    position: 'relative', // for absolutely-positioned wave
-    minHeight: 'calc(100vh - 64px)',
-    alignItems: 'center',
-    paddingTop: theme.spacing(10),
-    paddingBottom: theme.spacing(10),
-    [theme.breakpoints.down('md')]: {
-      flexDirection: 'column-reverse',
-      height: 'auto',
-      paddingTop: theme.spacing(3),
-      paddingBottom: theme.spacing(3),
-    },
+}))
+
+const DivHalfScreenLeft = styled('div')(({ theme }) => ({
+  width: '50%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  paddingLeft: '10%',
+
+  [theme.breakpoints.down(undefined)]: {
+    width: '47%',
+    paddingLeft: '7%',
   },
-  title: {
-    color: theme.palette.primary.main,
-  },
-  halfScreenLeft: {
-    width: '50%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    paddingLeft: '10%',
-    [theme.breakpoints.down(undefined)]: {
-      width: '47%',
-      paddingLeft: '7%',
-    },
-    [theme.breakpoints.down('md')]: {
-      width: '85%',
-      marginTop: theme.spacing(4),
-      paddingLeft: 0,
-      marginBottom: theme.spacing(8),
-    },
-  },
-  halfScreenRight: {
-    width: '50%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    position: 'absolute',
-    right: 0,
-    [theme.breakpoints.down('md')]: {
-      position: 'relative',
-      width: '100%',
-    },
-  },
-  subtitle: {
-    marginTop: theme.spacing(2),
-  },
-  buttonStyles: {
-    maxWidth: theme.spacing(30),
-    marginTop: theme.spacing(1.5),
-  },
-  wave: {
-    position: 'absolute',
-    bottom: -2, // avoids a gap appearing on some resolutions
-    width: '100%',
-    zIndex: -1,
-    [theme.breakpoints.down('md')]: {
-      display: 'none',
-    },
-  },
-  waveMobile: {
-    marginBottom: -2, // avoids a gap appearing on some resolutions
-    display: 'none',
-    position: 'static',
-    width: '100%',
-    zIndex: -1,
-    [theme.breakpoints.down('md')]: {
-      display: 'block',
-    },
-  },
-  installButton: {
+
+  [theme.breakpoints.down('md')]: {
+    width: '85%',
     marginTop: theme.spacing(4),
+    paddingLeft: 0,
+    marginBottom: theme.spacing(8),
+  },
+}))
+
+const DivHalfScreenRight = styled('div')(({ theme }) => ({
+  width: '50%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  position: 'absolute',
+  right: 0,
+
+  [theme.breakpoints.down('md')]: {
+    position: 'relative',
+    width: '100%',
+  },
+}))
+
+const TypographySubtitle = styled(Typography)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+}))
+
+const DivWave = styled('div')(({ theme }) => ({
+  position: 'absolute',
+
+  // avoids a gap appearing on some resolutions
+  bottom: -2,
+
+  width: '100%',
+  zIndex: -1,
+
+  [theme.breakpoints.down('md')]: {
+    display: 'none',
+  },
+}))
+
+const DivWaveMobile = styled('div')(({ theme }) => ({
+  // avoids a gap appearing on some resolutions
+  marginBottom: -2,
+
+  display: 'none',
+  position: 'static',
+  width: '100%',
+  zIndex: -1,
+
+  [theme.breakpoints.down('md')]: {
+    display: 'block',
   },
 }))
 
 function Landing({ landingData, causeId }) {
   const { title, subtitle, ctaImg } = landingData
-  const classes = useStyles()
   const theme = useTheme()
   const ctaImage = getImage(formatImg(ctaImg))
   return (
-    <div className="parent">
+    <div>
       <AppBar color="primary" position="sticky">
         <Toolbar>
-          <div className={classes.logoContainer}>
+          <DivLogoContainer>
             <div
               data-test-id="logo-container"
               style={{
@@ -123,36 +130,28 @@ function Landing({ landingData, causeId }) {
                 />
               </Link>
             </div>
-          </div>
-          <MoneyRaisedDisplay
-            whiteClassName={classes.whiteFont}
-            textVariant="subtitle2"
-            excludeText
-          />
+          </DivLogoContainer>
+          <MoneyRaisedDisplay textVariant="subtitle2" excludeText />
         </Toolbar>
       </AppBar>
-      <div className={classes.titleSection}>
-        <div className={classes.halfScreenLeft} data-test-id="title-wrapper">
+      <DivTitleSection>
+        <DivHalfScreenLeft data-test-id="title-wrapper">
           <Typography variant="h1" color="primary">
             {title}
           </Typography>
-          <Typography className={classes.subtitle}>{subtitle}</Typography>
-          <V4InstallButton
-            causeId={causeId}
-            buttonClassName={classes.installButton}
-            fullWidth
-          />
-        </div>
-        <div className={classes.halfScreenRight}>
+          <TypographySubtitle>{subtitle}</TypographySubtitle>
+          <V4InstallButton causeId={causeId} fullWidth />
+        </DivHalfScreenLeft>
+        <DivHalfScreenRight>
           <GatsbyImage image={ctaImage} alt="call to action" />
-        </div>
-        <div className={classes.wave}>
+        </DivHalfScreenRight>
+        <DivWave>
           <Wave color={theme.palette.primary.main} />
-        </div>
-      </div>
-      <div className={classes.waveMobile}>
+        </DivWave>
+      </DivTitleSection>
+      <DivWaveMobile>
         <Wave color={theme.palette.primary.main} />
-      </div>
+      </DivWaveMobile>
     </div>
   )
 }
