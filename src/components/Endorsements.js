d225a8d6730e7495ac3efa8a1c0266d5053fff1d
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-28 12:06:36 -0500

    Styled: Endorsements

--- src/components/Endorsements.js
+++ src/components/Endorsements.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
-import makeStyles from '@mui/styles/makeStyles'
+import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Paper from '@mui/material/Paper'
@@ -16,141 +16,137 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import V4InstallButton from './V4InstallButton'
 
-const useStyles = makeStyles((theme) => ({
-  logoContainer: { flex: 1, display: 'flex', flexDirection: 'row' },
-  wrapper: {
-    margin: '0 auto',
-    display: 'flex',
-    flexDirection: 'column',
-    paddingTop: theme.spacing(15.5),
-    width: '100%',
-    alignItems: 'center',
-    background: '#FAFAFA',
-    paddingBottom: theme.spacing(9),
-    [theme.breakpoints.down('md')]: {
-      paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
+const DivWrapper = styled('div')(({ theme }) => ({
+  margin: '0 auto',
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: theme.spacing(15.5),
+  width: '100%',
+  alignItems: 'center',
+  background: '#FAFAFA',
+  paddingBottom: theme.spacing(9),
+
+  [theme.breakpoints.down('md')]: {
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(6),
   },
-  reportsContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '86%',
-    marginTop: theme.spacing(4),
-    [theme.breakpoints.down(undefined)]: {
-      display: 'none',
-    },
+}))
+
+const DivRatingContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  background: '#FBF3E9',
+  padding: theme.spacing(1.5),
+  width: 'fitContent',
+  marginTop: theme.spacing(4),
+}))
+
+const DivStarsContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  margin: theme.spacing(0.5),
+}))
+
+const TypographyRatingsText = styled(Typography)(({ theme }) => ({
+  margin: theme.spacing(0.5),
+}))
+
+const DivTitleSectionWrapper = styled('div')(({ theme }) => ({
+  display: 'flex',
+  textAlign: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  width: '80%',
+}))
+
+const DivEndorserContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  width: '90%',
+  flexWrap: 'wrap',
+  marginTop: theme.spacing(9),
+  justifyContent: 'center',
+
+  [theme.breakpoints.down('md')]: {
+    marginTop: theme.spacing(7),
   },
-  ratingContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    background: '#FBF3E9',
-    padding: theme.spacing(1.5),
-    width: 'fitContent',
-    marginTop: theme.spacing(4),
-  },
-  starsContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    margin: theme.spacing(0.5),
-  },
-  ratingsText: {
-    margin: theme.spacing(0.5),
-  },
-  reportsSlider: {
-    display: 'none',
-    justifyContent: 'space-between',
-    width: '96%',
-    maxWidth: '800px',
-    marginTop: theme.spacing(4),
-    [theme.breakpoints.down(undefined)]: {
-      display: 'flex',
-    },
-  },
-  titleSectionWrapper: {
-    display: 'flex',
-    textAlign: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    width: '80%',
-  },
-  endorserContainer: {
-    display: 'flex',
+}))
+
+const DivEndorserTextContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '70%',
+
+  [theme.breakpoints.down('md')]: {
     width: '90%',
-    flexWrap: 'wrap',
-    marginTop: theme.spacing(9),
-    justifyContent: 'center',
-    [theme.breakpoints.down('md')]: {
-      marginTop: theme.spacing(7),
-    },
   },
-  endorserTextContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '70%',
-    [theme.breakpoints.down('md')]: {
-      width: '90%',
-    },
-  },
-  endorserImage: {
-    width: '240px',
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-  },
-  endorsementsSlider: {
-    width: '96%',
-    maxWidth: '1400px',
-    marginTop: theme.spacing(10),
-    marginBottom: theme.spacing(4),
-    display: 'flex',
-    alignItems: 'center',
-    [theme.breakpoints.down('md')]: {
-      marginTop: theme.spacing(4),
-    },
-  },
-  textSpacing: {
-    marginBottom: theme.spacing(2),
-  },
-  endorsementPaper: {
-    // react slick applies inline styling to children, this makes it look better
-    display: 'flex !important',
-    justifyContent: 'center',
-    padding: theme.spacing(1),
-  },
-  endorsementPaperTitleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 'fit-content',
-  },
-  Paper: {
-    width: 456,
-    height: 240,
-    display: 'flex',
-    flexDirection: 'column',
-    paddingTop: theme.spacing(2),
-    padddingBottom: theme.spacing(2),
-    paddingLeft: theme.spacing(2.5),
-    paddingRight: theme.spacing(2.5),
-    transition: 'transform .1s ease-in-out',
-    [theme.breakpoints.down('md')]: {
-      maxWidth: 340,
-    },
-  },
-  paperText: {
-    marginTop: theme.spacing(3),
-  },
-  installButton: {
+}))
+
+const GatsbyImageEndorserImage = styled(GatsbyImage)(({ theme }) => ({
+  width: '240px',
+  marginLeft: theme.spacing(2),
+  marginRight: theme.spacing(2),
+}))
+
+const DivEndorsementsSlider = styled('div')(({ theme }) => ({
+  width: '96%',
+  maxWidth: '1400px',
+  marginTop: theme.spacing(10),
+  marginBottom: theme.spacing(4),
+  display: 'flex',
+  alignItems: 'center',
+
+  [theme.breakpoints.down('md')]: {
     marginTop: theme.spacing(4),
   },
-  arrowButton: {
-    position: 'absolute',
-    right: '1%',
-    [theme.breakpoints.down('md')]: {
-      display: 'none',
-    },
+}))
+
+const TypographyTextSpacing = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+}))
+
+const DivEndorsementPaper = styled('div')(({ theme }) => ({
+  // react slick applies inline styling to children, this makes it look better
+  display: 'flex !important',
+
+  justifyContent: 'center',
+  padding: theme.spacing(1),
+}))
+
+const DivEndorsementPaperTitleContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  height: 'fit-content',
+}))
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  width: 456,
+  height: 240,
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: theme.spacing(2),
+  padddingBottom: theme.spacing(2),
+  paddingLeft: theme.spacing(2.5),
+  paddingRight: theme.spacing(2.5),
+  transition: 'transform .1s ease-in-out',
+
+  [theme.breakpoints.down('md')]: {
+    maxWidth: 340,
+  },
+}))
+
+const TypographyPaperText = styled(Typography)(({ theme }) => ({
+  marginTop: theme.spacing(3),
+}))
+
+const DivArrowButton = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  right: '1%',
+
+  [theme.breakpoints.down('md')]: {
+    display: 'none',
   },
 }))
 
@@ -184,54 +180,48 @@ function Endorsements({ endorsementsData, causeId }) {
     quote,
     smallEndorsements,
   } = endorsementsData
-  const classes = useStyles()
   const endorserImage = getImage(formatImg(endorserImg))
   const sliderRef = useRef()
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.titleSectionWrapper}>
+    <DivWrapper>
+      <DivTitleSectionWrapper>
         <Typography variant="h2" color="primary">
           {title}
         </Typography>
-        <div className={classes.ratingContainer}>
-          <div className={classes.starsContainer}>
+        <DivRatingContainer>
+          <DivStarsContainer>
             <Star style={{ color: '#ffc533', width: 18, height: 18 }} />
             <Star style={{ color: '#ffc533', width: 18, height: 18 }} />
             <Star style={{ color: '#ffc533', width: 18, height: 18 }} />
             <Star style={{ color: '#ffc533', width: 18, height: 18 }} />
             <StarHalf style={{ color: '#ffc533', width: 18, height: 18 }} />
-          </div>
-          <Typography className={classes.ratingsText}>
+          </DivStarsContainer>
+          <TypographyRatingsText>
             215,000+ people are Tabbing on Chrome
-          </Typography>
-        </div>
-      </div>
+          </TypographyRatingsText>
+        </DivRatingContainer>
+      </DivTitleSectionWrapper>
       {quote ? (
-        <div className={classes.endorserContainer}>
-          <GatsbyImage
+        <DivEndorserContainer>
+          <GatsbyImageEndorserImage
             alt="endorser image"
             image={endorserImage}
-            className={classes.endorserImage}
           />
-          <div className={classes.endorserTextContainer}>
-            <Typography
-              variant="h3"
-              color="primary"
-              className={classes.textSpacing}
-            >
+          <DivEndorserTextContainer>
+            <TypographyTextSpacing variant="h3" color="primary">
               {headerQuote}
-            </Typography>
-            <Typography variant="subtitle1" className={classes.textSpacing}>
+            </TypographyTextSpacing>
+            <TypographyTextSpacing variant="subtitle1">
               {quote}
-            </Typography>
+            </TypographyTextSpacing>
             <Typography variant="caption" color="primary">
               {endorser}
             </Typography>
             <Typography>{endorserTitle}</Typography>
-          </div>
-        </div>
+          </DivEndorserTextContainer>
+        </DivEndorserContainer>
       ) : null}
-      <div className={classes.endorsementsSlider}>
+      <DivEndorsementsSlider>
         <Slider
           dots
           ref={sliderRef}
@@ -255,12 +245,9 @@ function Endorsements({ endorsementsData, causeId }) {
           ]}
         >
           {smallEndorsements.map((endorsement) => (
-            <div
-              className={classes.endorsementPaper}
-              key={endorsement.endorser}
-            >
-              <Paper className={classes.Paper} elevation={3}>
-                <div className={classes.endorsementPaperTitleContainer}>
+            <DivEndorsementPaper key={endorsement.endorser}>
+              <StyledPaper elevation={3}>
+                <DivEndorsementPaperTitleContainer>
                   <GatsbyImage
                     alt="endorsement image"
                     image={getImage(formatImg(endorsement.img))}
@@ -274,28 +261,25 @@ function Endorsements({ endorsementsData, causeId }) {
                   <Typography variant="caption" color="primary">
                     {endorsement.endorser}
                   </Typography>
-                </div>
-                <Typography className={classes.paperText}>
+                </DivEndorsementPaperTitleContainer>
+                <TypographyPaperText>
                   {endorsement.endorsement}
-                </Typography>
-              </Paper>
-            </div>
+                </TypographyPaperText>
+              </StyledPaper>
+            </DivEndorsementPaper>
           ))}
         </Slider>
-        <div className={classes.arrowButton}>
+        <DivArrowButton>
           <IconButton
             onClick={() => sliderRef.current.slickNext()}
             size="large"
           >
             <Arrow />
           </IconButton>
-        </div>
-      </div>
-      <V4InstallButton
-        causeId={causeId}
-        buttonClassName={classes.installButton}
-      />
-    </div>
+        </DivArrowButton>
+      </DivEndorsementsSlider>
+      <V4InstallButton causeId={causeId} />
+    </DivWrapper>
   )
 }
 
