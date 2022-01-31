83b633d44f17f89e46ab78e3b607b80f8344a028
Kevin Jennison <kevin.jennison1@gmail.com>
2022-01-30 21:20:55 -0500

    Remove separate stories for viewports

--- src/components/ComingSoon.stories.jsx
+++ src/components/ComingSoon.stories.jsx
@@ -2,7 +2,11 @@
 import React from 'react'
 import { useCauseData } from 'src/utils/storybookHelpers/useCauseData'
 import ComingSoon from './ComingSoon'
-import { mobile, seas, cats } from '../../.storybook/boilerPlate'
+import {
+  seas,
+  cats,
+  defaultChromaticViewports,
+} from '../../.storybook/boilerPlate'
 
 export default {
   title: 'Pages/ComingSoon',
@@ -13,83 +17,49 @@ function Template(_args, { loaded: { data } }) {
   return <ComingSoon location="/" pageContext={data} />
 }
 
-export const MobileSeas = mobile(seas(Template.bind({})))
-/*
- * loaders are experimental and allow us to use async await which we need in
- * order to programatically spoof gatsby images
- * https://storybook.js.org/docs/react/writing-stories/loaders
- */
-MobileSeas.loaders = [
+export const SeasCause = seas(Template.bind({}))
+
+SeasCause.loaders = [
   async () => {
     const data = await useCauseData('seas')
     delete data.data.causeLaunch.launchDate
     return { data }
   },
 ]
-MobileSeas.parameters = {
+SeasCause.parameters = {
   viewport: {
     defaultViewport: 'mobile2',
   },
-  chromatic: { viewports: [414, 900] },
+  chromatic: { viewports: [...defaultChromaticViewports] },
 }
 
-export const MobileCats = mobile(cats(Template.bind({})))
-MobileCats.loaders = [
+export const CatsCause = cats(Template.bind({}))
+CatsCause.loaders = [
   async () => ({
     data: await useCauseData('cats'),
   }),
 ]
-MobileCats.parameters = {
+CatsCause.parameters = {
   viewport: {
     defaultViewport: 'mobile2',
   },
-  chromatic: { viewports: [414, 900], disableSnapshot: true },
-}
-
-export const FullWidthSeas = seas(Template.bind({}))
-FullWidthSeas.loaders = [
-  async () => ({
-    data: await useCauseData('seas'),
-  }),
-]
-FullWidthSeas.parameters = {}
-
-export const FullWidthCats = cats(Template.bind({}))
-FullWidthCats.loaders = [
-  async () => ({
-    data: await useCauseData('cats'),
-  }),
-]
-FullWidthCats.parameters = {
-  chromatic: { disableSnapshot: true },
-}
-
-export const FutureMobileCats = mobile(cats(Template.bind({})))
-FutureMobileCats.loaders = [
-  async () => {
-    const data = await useCauseData('cats')
-    data.data.causeLaunch.launchDate = new Date(Date.now() + 5 * 86400000)
-    return { data }
+  chromatic: {
+    viewports: [...defaultChromaticViewports],
+    disableSnapshot: true,
   },
-]
-FutureMobileCats.parameters = {
-  viewport: {
-    defaultViewport: 'mobile2',
-  },
-  chromatic: { viewports: [414, 900], disableSnapshot: true },
 }
 
-export const FutureMobileSeas = mobile(seas(Template.bind({})))
-FutureMobileSeas.loaders = [
+export const FutureSeasCause = seas(Template.bind({}))
+FutureSeasCause.loaders = [
   async () => {
     const data = await useCauseData('seas')
     data.data.causeLaunch.launchDate = new Date(Date.now() + 5 * 86400000)
     return { data }
   },
 ]
-FutureMobileSeas.parameters = {
+FutureSeasCause.parameters = {
   viewport: {
     defaultViewport: 'mobile2',
   },
-  chromatic: { viewports: [414, 900] },
+  chromatic: { viewports: [...defaultChromaticViewports] },
 }
--- src/components/Mission.stories.jsx
+++ src/components/Mission.stories.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useCauseData } from 'src/utils/storybookHelpers/useCauseData'
 import Mission from './Mission'
-import { mobile, seas, cats } from '../../.storybook/boilerPlate'
+import {
+  seas,
+  cats,
+  defaultChromaticViewports,
+} from '../../.storybook/boilerPlate'
 
 export default {
   title: 'Components/Mission',
@@ -12,51 +16,32 @@ function Template(_args, { loaded: { data } }) {
   const missionData = data.data.sections.Mission
   return <Mission missionData={missionData} causeId={data.data.causeId} />
 }
-export const MobileSeas = mobile(seas(Template.bind({})))
-/*
- * loaders are experimental and allow us to use async await which we need in
- * order to programatically spoof gatsby images
- * https://storybook.js.org/docs/react/writing-stories/loaders
- */
-MobileSeas.loaders = [
+export const SeasCause = seas(Template.bind({}))
+
+SeasCause.loaders = [
   async () => ({
     data: await useCauseData('seas'),
   }),
 ]
-MobileSeas.parameters = {
+SeasCause.parameters = {
   viewport: {
     defaultViewport: 'mobile2',
   },
-  chromatic: { viewports: [414, 736] },
+  chromatic: { viewports: [...defaultChromaticViewports] },
 }
 
-export const MobileCats = mobile(cats(Template.bind({})))
-MobileCats.loaders = [
+export const CatsCause = cats(Template.bind({}))
+CatsCause.loaders = [
   async () => ({
     data: await useCauseData('cats'),
   }),
 ]
-MobileCats.parameters = {
+CatsCause.parameters = {
   viewport: {
     defaultViewport: 'mobile2',
   },
-  chromatic: { viewports: [414, 736], disableSnapshot: true },
-}
-
-export const FullWidthSeas = seas(Template.bind({}))
-FullWidthSeas.loaders = [
-  async () => ({
-    data: await useCauseData('seas'),
-  }),
-]
-FullWidthSeas.parameters = {}
-
-export const FullWidthCats = cats(Template.bind({}))
-FullWidthCats.loaders = [
-  async () => ({
-    data: await useCauseData('cats'),
-  }),
-]
-FullWidthCats.parameters = {
-  chromatic: { disableSnapshot: true },
+  chromatic: {
+    viewports: [...defaultChromaticViewports],
+    disableSnapshot: true,
+  },
 }
